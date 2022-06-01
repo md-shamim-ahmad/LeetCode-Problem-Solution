20852666f1c9e1e7f12a81c69fb5c3d848bcc64a
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-01 16:29:13 +0600

    Median of Two Sorted Arrays.cpp

---
+++ LeetCode Problems/1 to 100/4. Median of Two Sorted Arrays.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int> nums1, vector<int>& nums2) {
+        for (int i = 0; i < nums2.size(); i++)
+            nums1.push_back(nums2[i]);
+        sort(nums1.begin(), nums1.end());
+        if (nums1.size() & 1)
+            return nums1[nums1.size() / 2];
+        else 
+            return (double)(nums1[nums1.size() / 2 - 1] + nums1[nums1.size() / 2]) / 2;
+    }
+};
