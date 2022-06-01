a678c54bb22ea18787f4523eeae66a7d43d1da18
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-01 16:29:52 +0600

    Reverse Integer.cpp

---
+++ LeetCode Problems/1 to 100/7. Reverse Integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long rev_x = 0;
+        while(x != 0) {
+            int rem = x % 10;
+            rev_x = rev_x * 10 + rem;
+            x /= 10;
+            if (rev_x > INT_MAX || rev_x < INT_MIN)
+                return 0;
+        }
+        return rev_x;
+    }
+};
