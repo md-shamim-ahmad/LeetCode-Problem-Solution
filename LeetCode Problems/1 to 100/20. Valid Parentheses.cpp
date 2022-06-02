007da95eea79561e80e43cba8ef42d5f4ce3a124
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-02 11:19:08 +0600

    Create 20. Valid Parentheses.cpp

---
+++ LeetCode Problems/1 to 100/20. Valid Parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        // if(s.size() == 1)
+        //     return false;
+        stack<char> arr;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                arr.push(s[i]);
+                continue;
+            }
+            if (arr.size() == 0)
+                return false;
+            if (s[i] == ')' && arr.top() == '(') {                
+                arr.pop();
+            }
+            else if (s[i] == '}' && arr.top() == '{') {
+                arr.pop();
+            }
+            else if (s[i] == ']' && arr.top() == '[') {
+                arr.pop();
+            }
+            else 
+                return false;
+        }
+        return arr.size() == 0 ? true : false;
+    }
+};
