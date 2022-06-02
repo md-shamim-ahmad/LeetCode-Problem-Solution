44c12bd67d7c64d4950a6aa051fec739fff67c4a
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-02 11:18:54 +0600

    Create 14. Longest Common Prefix.cpp

---
+++ LeetCode Problems/1 to 100/14. Longest Common Prefix.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    struct node {
+        char info;
+        int cnt;
+        node *list[26];
+    };
+    node *newNode(char info) {
+        node *temp = new node();
+        temp->cnt = 0;
+        temp->info = info;
+        return temp;
+    }
+    void insert(node* &head, string s) {
+        node* temp = head;
+        for (int i = 0; i < s.size(); i++) {
+            int index = (int)s[i] - 'a';
+            if (!temp->list[index]) {
+                temp->list[index] = newNode(s[i]);
+            }
+            temp = temp->list[index];
+        }
+        temp->cnt = 1;
+    }
+    void display(node *root) {
+        if(!root)
+            return;
+        node *temp = root;
+        cout << temp->info << " " << temp->cnt << '\n';
+        for (int i = 0; i < 26; i++)
+            display(temp->list[i]);
+    }
+    int countChild(node *temp, int &index) {
+        int cnt = 0;
+        for (int i = 0; i < 26; i++) {
+            if (temp->list[i] != nullptr) {
+                cnt++;
+                index = i;
+            }
+        }
+        return cnt;
+    }
+    string longestCommonPrefix(vector<string>& strs) {
+        node *head = new node();
+        for (int i = 0; i < strs.size(); i++) {
+            insert(head, strs[i]);
+        }
+        node *curr = head;
+        string lcp;
+        int index;
+        while (!curr->cnt && countChild(curr, index) == 1){
+            curr = curr->list[index];
+            lcp += 'a' + index;
+        }
+        return lcp;
+    }
+};
