2558d3b69ed884a3650b479733cef90795877b3e
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-03 16:12:37 +0600

    Create 33. Search in Rotated Sorted Array.cpp

---
+++ LeetCode Problems/1 to 100/33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == target)
+                return i;
+        }
+        return -1;
+    }
+};
