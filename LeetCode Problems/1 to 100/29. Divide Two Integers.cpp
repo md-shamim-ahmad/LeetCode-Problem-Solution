6aaf937b1503706db27f167d46ac95b18a4d1466
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-03 16:12:27 +0600

    Create 29. Divide Two Integers.cpp

---
+++ LeetCode Problems/1 to 100/29. Divide Two Integers.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend < 0 && divisor < 0) {
+            if (divisor == INT_MIN)
+                divisor += 1;
+            divisor *= -1;
+            if (dividend == INT_MIN)
+                dividend += 1;
+            dividend *= -1;            
+        }
+        return (dividend / divisor);
+    }
+};
