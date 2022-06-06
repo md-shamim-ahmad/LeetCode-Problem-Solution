0058eb04d7742b3d23331f004fb0a13a7d526ef1
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-06 06:45:59 +0600

    Create 155. Min Stack.cpp

---
+++ LeetCode Problems/101 to 200/155. Min Stack.cpp
@@ -0,0 +1,33 @@
+class MinStack {
+public:
+    void push(int val) {
+        int min_idx = size(arr);
+        if (!empty(arr) && getMin() < val) {
+            min_idx = arr.back().second;
+        }
+        arr.emplace_back(val, min_idx);
+    }
+    
+    void pop() {
+        arr.pop_back();
+    }
+    
+    int top() {
+        return arr.back().first;
+    }
+    
+    int getMin() {
+        return arr[arr.back().second].first;
+    }
+private:        
+        vector<pair<int, int>> arr;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
