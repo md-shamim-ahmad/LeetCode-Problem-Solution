89367f9da328811e15251c09c368205e956ee45c
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-06 06:45:48 +0600

    Create 151. Reverse Words in a String .cpp

---
+++ LeetCode Problems/101 to 200/151. Reverse Words in a String .cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string> words;
+        string str = "";
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != ' ') {
+                str += s[i];
+            } else if (s[i] == ' ' && str.size() > 0) {
+                words.push_back(str);
+                str = "";
+            }
+        }
+        if (str.size() > 0) {
+            words.push_back(str);
+        }
+        reverse(words.begin(), words.end());
+        str = words[0];
+        for (int i = 1; i < words.size(); i++) {
+            str += " "; str += words[i];
+        }
+        return str;
+    }
+};
