0652dc462aed07c3ee0430b0deda711f2b1a3657
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-07 21:15:06 +0600

    Create 189. Rotate Array.cpp

---
+++ LeetCode Problems/101 to 200/189. Rotate Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k %= nums.size();
+        vector<int> arr;
+        for (int i = nums.size() - k; i < nums.size(); i++)
+            arr.push_back(nums[i]);
+        for (int i = 0; i < nums.size() - k; i++)
+            arr.push_back(nums[i]);
+        for (int i = 0; i < nums.size(); i++) {
+            nums[i] = arr[i];
+        }
+    }
+};
