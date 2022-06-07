258226839302f528a376dd892d9201ad8d40d013
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-07 21:16:50 +0600

    Create 268. Missing Number.cpp

---
+++ LeetCode Problems/201 to 300/268. Missing Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = 0;
+        for(auto num : nums)
+            sum += num;        
+        int numsLength = nums.size();
+        int total = numsLength*(numsLength+1)/2;    
+        return total-sum;
+    }
+};
