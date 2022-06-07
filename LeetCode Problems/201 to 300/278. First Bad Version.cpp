8c6b5596124314dad6f54a01dca9f16ad91ad6c2
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-07 21:16:54 +0600

    Create 278. First Bad Version.cpp

---
+++ LeetCode Problems/201 to 300/278. First Bad Version.cpp
@@ -0,0 +1,22 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {    
+public:
+    int search(long l, long r) {
+        while (l <= r) {
+            long long m = (l + r) / 2;
+            if (isBadVersion(m) && !isBadVersion(m - 1)) {
+                return (int)m;
+            }
+            if (!isBadVersion(m)) {
+                return search(m + 1, r);
+            }
+            return search(l, m);
+        }
+        return l;
+    }
+    int firstBadVersion(int n) {
+        return search(1, (long long)n);
+    }
+};
