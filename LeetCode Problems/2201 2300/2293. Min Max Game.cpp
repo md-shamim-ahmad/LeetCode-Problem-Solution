77584903c230b30ac3180eabb6200a6cb9ad695b
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-13 09:04:35 +0600

    Create 2293. Min Max Game.cpp

---
+++ LeetCode Problems/2201 2300/2293. Min Max Game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minMaxGame(vector<int>& nums) {
+        int n = nums.size();
+        while (n != 1) {
+            vector<int> arr;
+            int k = 0;
+            for (int i = 0; i < nums.size() / 2; i++) {
+                if (i % 2 == 0) {
+                    arr.push_back(min(nums[2 * i], nums[2 * i + 1]));
+                }else {
+                    arr.push_back(max(nums[2 * i], nums[2 * i + 1]));
+                }
+            }
+            n /= 2;
+            nums=arr;
+        }
+        return nums[0];
+    }
+};
