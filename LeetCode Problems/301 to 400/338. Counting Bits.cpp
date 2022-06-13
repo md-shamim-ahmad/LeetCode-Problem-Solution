03ec9a75e06fcfaf2afbff3d468cd3229e004162
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-13 09:04:06 +0600

    Create 338. Counting Bits.cpp

---
+++ LeetCode Problems/301 to 400/338. Counting Bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int binaryNum(int n) {
+        int res = 0;
+        while (n != 0) {
+            int r = n % 2;
+            res += r;
+            n /= 2;
+        }
+        return res;
+    }
+    vector<int> countBits(int n) {
+        vector<int> ans(n + 1);
+        for (int i= 0; i <= n; i++) {
+            ans[i] = binaryNum(i);
+        }
+        return ans;
+    }
+};
