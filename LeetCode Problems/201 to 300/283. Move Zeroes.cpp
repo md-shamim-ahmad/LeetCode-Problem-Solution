61f074486f335ed33d7abb3ff2b3286d27fc8e56
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-13 09:02:21 +0600

    Create 283. Move Zeroes.cpp

---
+++ LeetCode Problems/201 to 300/283. Move Zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size(), cnt = 0;
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0)
+                cnt++;
+            else
+                ans.push_back(nums[i]);
+        }
+        for (int i = 0; i < cnt; i++)
+            ans.push_back(0);
+        nums = ans;
+    }
+};
