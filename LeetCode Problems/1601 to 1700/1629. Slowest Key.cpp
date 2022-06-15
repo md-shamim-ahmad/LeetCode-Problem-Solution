15e155d27ea4021f8beec3c3174c4aa51626a399
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-15 08:21:18 +0600

    Create 1629. Slowest Key.cpp

---
+++ LeetCode Problems/1601 to 1700/1629. Slowest Key.cpp
@@ -0,0 +1,22 @@
+bool SortBaseOnFirstElement(const pair<char, int> &a, const pair<char, int> &b) {
+    return a.first < b.first;
+}
+
+bool SortBaseOnSecondElement(const pair<char, int> &a, const pair<char, int> &b) {
+    return a.second < b.second;
+}
+class Solution {
+public:
+
+    char slowestKey(vector<int>& releaseTimes, string keysPressed) {
+        int n = keysPressed.size();
+        vector<pair<char, int>> arr;
+        arr.emplace_back(keysPressed[0], releaseTimes[0]);
+        for (int i = 1; i < n; i++) {
+            arr.emplace_back(keysPressed[i], releaseTimes[i] - releaseTimes[i - 1]);
+        }
+        sort(arr.begin(), arr.end(), SortBaseOnFirstElement);
+        sort(arr.begin(), arr.end(), SortBaseOnSecondElement);
+        return arr[n - 1].first;
+    }
+};
