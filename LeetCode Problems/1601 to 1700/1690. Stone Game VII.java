67a00e49ef6efbeadba718b8f58dbec61d605a9d
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-15 08:21:28 +0600

    Create 1690. Stone Game VII.java

---
+++ LeetCode Problems/1601 to 1700/1690. Stone Game VII.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int stoneGameVII(int[] S) {
+        int N = S.length;
+        int[] dpCurr = new int[N], dpLast = new int[N];
+        for (int i = N - 2; i >= 0; i--) {
+            int total = S[i];
+            int[] temp = dpLast;
+            dpLast = dpCurr;
+            dpCurr = temp;
+            for (int j = i + 1; j < N; j++) {
+                total += S[j];
+                dpCurr[j] = Math.max(total - S[i] - dpLast[j], total - S[j] - dpCurr[j-1]);
+            }
+        }
+        return dpCurr[N-1];
+    }
+}
