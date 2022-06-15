f65f2c0593e6846f9b049ac37668db19ccc3dea5
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-15 08:21:36 +0600

    Create 1695. Maximum Erasure Value.cpp

---
+++ LeetCode Problems/1601 to 1700/1695. Maximum Erasure Value.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        unordered_set<int> mp;
+        int curr_sum = 0, sum = 0, i = 0, j = 0;
+        while(i < nums.size() && j < nums.size()) {
+            if (mp.find(nums[i]) == mp.end()) {
+                mp.insert(nums[i]);
+                curr_sum += nums[i];
+                sum = max(curr_sum, sum);
+                i++;
+            } else {
+                mp.erase(nums[j]);
+                curr_sum -= nums[j];
+                j++;
+            }            
+        }
+        return sum;
+    }
+};
