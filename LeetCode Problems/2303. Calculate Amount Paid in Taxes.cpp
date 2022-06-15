76bfeda3b095758a7a40647073d864e1c6f32c89
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-15 08:21:46 +0600

    Create 2303. Calculate Amount Paid in Taxes.cpp

---
+++ LeetCode Problems/2303. Calculate Amount Paid in Taxes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double calculateTax(vector<vector<int>>& brackets, int income) {
+        double tax = 0;
+        int prev = 0;
+        for (auto it : brackets) {
+            int rep = min(income, it[0]);
+            tax += double(rep - prev) * (double)it[1] / 100.0;
+            prev = it[0];
+            if (income <= it[0])
+                break;
+        }
+        return tax;
+    }
+};
