e8c7a7d01de2c51ec5793b6994d25537c039d9e1
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-15 08:21:21 +0600

    Create 1658. Minimum Operations to Reduce X to Zero.cpp

---
+++ LeetCode Problems/1601 to 1700/1658. Minimum Operations to Reduce X to Zero.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum < x) return -1;
+        if (sum == x) return nums.size();  
+        int diff = sum - x, j = 0, ans = 0, curr_sum = 0;              
+        for (int i = 0; i < nums.size(); i++) {
+            curr_sum += nums[i];
+            while (curr_sum > diff) {
+                curr_sum -= nums[j++];
+            }
+            if (curr_sum == diff)
+                ans = max(ans, i - j + 1);
+        }
+        return (ans == 0) ? -1 : nums.size() - ans;
+    }
+};
