2f7409c4b3ec46f6b51d28ed147b3e1e9d3c1d71
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-19 21:23:57 +0600

    Create 1022. Sum of Root To Leaf Binary Numbers.cpp

---
+++ LeetCode Problems/1001 to 1100/1022. Sum of Root To Leaf Binary Numbers.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int help(TreeNode* root, int value) {
+        if (root == nullptr)
+            return 0;
+        value = (value * 2 + root->val);
+        if (root->left == root->right) {
+            return value;
+        } else {
+            return help(root->left, value) + help(root->right, value);
+        }
+    }
+    int sumRootToLeaf(TreeNode* root) {        
+        return help (root, 0);
+    }
+};
