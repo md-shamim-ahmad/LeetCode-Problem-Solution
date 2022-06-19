5e3cd603404f6d1e872bd2b4040ea8bd9a6abe8e
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-19 21:24:09 +0600

    Create 1089. Duplicate Zeros.cpp

---
+++ LeetCode Problems/1001 to 1100/1089. Duplicate Zeros.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int n = arr.size(), num = 0, cnt = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (!arr[i]) {
+                for (int j = n - 1; j > i + 1; j--)
+                    arr[j] = arr[j - 1];
+                arr[i + 1] = arr[i];
+                i++;
+            }
+        }
+    }
+};
