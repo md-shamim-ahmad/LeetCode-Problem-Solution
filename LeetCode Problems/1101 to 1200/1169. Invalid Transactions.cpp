cf3dba688c4a08e595aaa5f7a988076192eaf164
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-19 21:24:15 +0600

    Create 1169. Invalid Transactions.cpp

---
+++ LeetCode Problems/1101 to 1200/1169. Invalid Transactions.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> invalidTransactions(vector<string> &transactions) {
+        vector<bool> invalid(transactions.size(), false);
+        vector<vector<string>> trans;
+        for (auto t: transactions) {
+            string tmp;
+            vector<string> tt;
+            for (auto c:t) {
+                if (c == ',') {
+                    tt.push_back(tmp);
+                    tmp = "";
+                } else
+                    tmp += c;
+            }
+            tt.push_back(tmp);
+            trans.push_back(tt);
+        }
+        for (int i = 0; i < trans.size(); i++) {
+            if (stoi(trans[i][2]) > 1000) invalid[i] = true;
+            for (int j = i + 1; j < trans.size(); j++) {
+                if ((trans[i][0] == trans[j][0]) && abs(stoi(trans[i][1]) - stoi(trans[j][1])) <= 60 &&
+                    (trans[i][3] != trans[j][3])) {
+                    invalid[i] = true;
+                    invalid[j] = true;
+
+                }
+            }
+        }
+
+        vector<string> res;
+        for (int i = 0; i < trans.size(); i++) {
+            if (invalid[i]) res.push_back(transactions[i]);
+        }
+        return res;
+    }
+};
