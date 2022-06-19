b0719639a3abf8b67abf8b32395baefe797247a5
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-19 21:24:00 +0600

    Create 1029. Two City Scheduling.cpp

---
+++ LeetCode Problems/1001 to 1100/1029. Two City Scheduling.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>> &cs, int res = 0) {
+        nth_element(begin(cs), begin(cs) + cs.size() / 2, end(cs), [](vector<int> &a, vector<int> &b) {
+            return (a[0] - a[1] < b[0] - b[1]);
+        });
+        for (auto i = 0; i < cs.size() / 2; ++i) {
+            res += cs[i][0] + cs[i + cs.size() / 2][1];
+        }
+        return res;
+    }
+};
