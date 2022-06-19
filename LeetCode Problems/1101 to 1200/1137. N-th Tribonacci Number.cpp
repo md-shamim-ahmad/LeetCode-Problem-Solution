8369f99dd11936774678d36abb338bc82fc94f80
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-19 21:24:11 +0600

    Create 1137. N-th Tribonacci Number.cpp

---
+++ LeetCode Problems/1101 to 1200/1137. N-th Tribonacci Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        vector<int> dp(n + 3);
+        dp[0] = 0; dp[1] = dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+        }
+        return dp[n];
+    }
+};
