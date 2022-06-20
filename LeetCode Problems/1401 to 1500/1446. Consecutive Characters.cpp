05425969f9ef8ef2cb70d4eb14ef9ee355efd7ce
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-20 22:05:45 +0600

    Create 1446. Consecutive Characters.cpp

---
+++ LeetCode Problems/1401 to 1500/1446. Consecutive Characters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxPower(string s) {
+        int ans = 1;
+        for (int i = 0; i < s.size() - 1;) {
+            if (s[i] == s[i + 1]) {
+                int cnt = 1;
+                while (s[i] == s[i + 1]) {
+                    cnt++, i++;
+                }
+                ans = max(ans, cnt);
+            } else i++;
+        }
+        return ans;
+    }
+};
