5f9e1134231add2881a80b1baf57ae35c4024131
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-20 22:05:34 +0600

    Create 1299. Replace Elements with Greatest Element on Right Side.cpp

---
+++ LeetCode Problems/1201 to 1300/1299. Replace Elements with Greatest Element on Right Side.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        int val = -1;
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            int temp = arr[i];
+            arr[i] = val;
+            val = max(arr[i], temp);
+        }
+        return arr;
+    }
+};
