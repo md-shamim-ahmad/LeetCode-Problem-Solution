b6f8e5275032f688791c63fe76394546c7b20c8e
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-26 15:50:40 +0600

    Create 2023. Number of Pairs of Strings With Concatenation Equal to Target.cpp

---
+++ LeetCode Problems/2001 to 2100/2023. Number of Pairs of Strings With Concatenation Equal to Target.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numOfPairs(vector<string>& nums, string target) {
+        int cnt = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums.size(); j++) {
+                if (i == j)
+                    continue;
+                if (nums[i] + nums[j] == target)
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+};
