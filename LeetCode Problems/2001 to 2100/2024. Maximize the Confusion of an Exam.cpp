5066ede549f3c0b949750969b09e9800d627719a
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-26 15:50:41 +0600

    Create 2024. Maximize the Confusion of an Exam.cpp

---
+++ LeetCode Problems/2001 to 2100/2024. Maximize the Confusion of an Exam.cpp
@@ -0,0 +1,32 @@
+class Solution {    
+public:
+    int findLen(string& A, int n, int k, char ch) {
+        int maxlen = 1;
+        int cnt = 0;
+        int l = 0, r = 0;     
+        while (r < n) {
+            if (A[r] != ch)
+                ++cnt;
+            while (cnt > k) {
+                if (A[l] != ch)
+                    --cnt;
+                ++l;
+            }
+            maxlen = max(maxlen, r - l + 1);
+            ++r;
+        }
+        return maxlen;
+    }
+
+    int answer(string& A, int n, int k) {
+        int maxlen = 1;
+        for (int i = 0; i < 26; ++i) {
+            maxlen = max(maxlen, findLen(A, n, k, i+'A'));
+            maxlen = max(maxlen, findLen(A, n, k, i+'a'));
+        }
+        return maxlen;
+    }
+    int maxConsecutiveAnswers(string A, int k) {
+        return answer(A, A.size(), k);
+    }
+};
