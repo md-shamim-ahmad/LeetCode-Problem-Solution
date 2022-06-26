f0ab12e06d58217c4295464266bc1679a728ed34
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-26 15:51:26 +0600

    Create 2279. Maximum Bags With Full Capacity of Rocks.cpp

---
+++ LeetCode Problems/2201 to 2300/2279. Maximum Bags With Full Capacity of Rocks.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        map<int, int> mp;
+        for (int i = 0; i < capacity.size(); i++) {
+        	mp[capacity[i] - rocks[i]]++;
+        }
+        int cnt = 0;
+        for (auto it : mp) {
+            cout << it.first << " " << it.second << '\n';
+            if (it.first == 0) {
+                cnt += it.second;
+            }else {
+                if (it.first * it.second < additionalRocks) {
+                    cnt += it.second;
+                    additionalRocks -= it.first * it.second;
+                } else {
+                    cnt += additionalRocks / it.first;
+                    break;
+                }
+            }
+        }
+        cout << additionalRocks << '\n';
+        return cnt;
+    }
+};
