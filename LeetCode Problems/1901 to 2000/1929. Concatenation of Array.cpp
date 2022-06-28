082405315b42c30e8830deabc287bae5526755ad
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-28 18:19:09 +0600

    Create 1929. Concatenation of Array.cpp

---
+++ LeetCode Problems/1901 to 2000/1929. Concatenation of Array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> arr(2 * nums.size());
+        for (int i = 0; i < nums.size(); i++) {
+            arr[i] = nums[i];
+            arr[i + nums.size()] = nums[i];
+        }
+        return arr;
+    }
+};
