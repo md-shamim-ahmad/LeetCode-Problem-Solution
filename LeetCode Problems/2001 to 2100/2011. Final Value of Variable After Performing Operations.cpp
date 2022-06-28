e8a87f1fdb551818086a9f87b37eaa5b3e9b110c
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-28 18:19:24 +0600

    Create 2011. Final Value of Variable After Performing Operations.cpp

---
+++ LeetCode Problems/2001 to 2100/2011. Final Value of Variable After Performing Operations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int cnt = 0;
+        for (int i = 0; i < operations.size(); i++) {
+            if (operations[i] == "++X") {
+                ++cnt;
+            } else if (operations[i] == "X++") {
+                cnt++;
+            } else if (operations[i] == "--X") {
+                --cnt;
+            }else if (operations[i] == "X--") {
+                cnt--;
+            }
+        }
+        return cnt;
+    }    
+};
