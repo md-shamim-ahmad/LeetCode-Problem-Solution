e762945d00ed638066c9eca3e27815b4ed7b0492
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-28 18:19:12 +0600

    Create 2001. Number of Pairs of Interchangeable Rectangles.cpp

---
+++ LeetCode Problems/2001 to 2100/2001. Number of Pairs of Interchangeable Rectangles.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long interchangeableRectangles(vector<vector<int>>& rectangles) {
+        map<double, long long> ratio;//(rectangles.size());
+        for (int i = 0; i < rectangles.size(); i++) {
+            double cal = (double)rectangles[i][0] / rectangles[i][1];
+            ratio[cal]++;        
+        }
+        long long cnt = 0;
+        for (auto it : ratio) {
+            if (it.second > 1) {
+                cnt += ((1 + (it.second - 1)) * (it.second - 1)) / 2;
+            }
+        }
+        return cnt;
+    }
+};
