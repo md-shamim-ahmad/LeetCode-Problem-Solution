ccca450015b591f7fbd31f9f756442cfbcd9ad0f
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-30 19:44:11 +0600

    Create 541. Reverse String II.java

---
+++ LeetCode Problems/501 to 600/541. Reverse String II.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public String reverseStr(String s, int k) {
+        if (s == null || s.length() == 0 || k == 0) {
+            return "";
+        }
+        int i = 0;
+        StringBuilder sb = new StringBuilder(s);
+        while (i < s.length()) {
+            int left = i;
+            int right = Math.min(i + k - 1, s.length() - 1);
+            while (left < right) {
+                char a = sb.charAt(left);
+                char b = sb.charAt(right);
+                sb.setCharAt(left, b);
+                sb.setCharAt(right, a);
+                left ++;
+                right --;
+            }
+            i = i + 2 * k;
+        }
+        return sb.toString();
+    }
+}
