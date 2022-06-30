dd9a9ef25a862027a4ccf1cb92845b5a362006b5
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-06-30 19:44:08 +0600

    Create 535. Encode and Decode TinyURL.cpp

---
+++ LeetCode Problems/501 to 600/535. Encode and Decode TinyURL.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    string url;
+public:
+
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        url = longUrl;
+        return url;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return url;
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
