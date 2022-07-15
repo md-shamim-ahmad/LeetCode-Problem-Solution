474431150b25db1234657f126e5f44d9f2c2b518
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-07-15 16:13:46 +0600

    Create 729. My Calendar I.cpp

---
+++ LeetCode Problems/701 to 800/729. My Calendar I.cpp
@@ -0,0 +1,18 @@
+class MyCalendar {
+    vector<pair<int, int>> dic;
+public:    
+    bool book(int start, int end) {
+        for (auto it : dic) {
+            if (max(it.first, start) < min(it.second, end))
+                return false;            
+        }
+        dic.push_back({start, end});
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
