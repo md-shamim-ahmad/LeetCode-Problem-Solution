b43ccf50334796072486760385456f3c498fbb3b
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-07-15 16:14:03 +0600

    Create 848. Shifting Letters.cpp

---
+++ LeetCode Problems/801 to 900/848. Shifting Letters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        for (int i = shifts.size() - 2; i >= 0; i--) {
+            shifts[i] =(shifts[i] % 26 + shifts[i + 1] % 26) % 26;
+        }
+        for (int i = 0; i < s.size(); i++) {
+            int int_val = (int) s[i] + (shifts[i] % 26);
+            if (int_val > 122) {
+                int_val -= 26;
+            }
+            s[i] = (char) int_val;
+        }
+        return s;
+    }
+};
