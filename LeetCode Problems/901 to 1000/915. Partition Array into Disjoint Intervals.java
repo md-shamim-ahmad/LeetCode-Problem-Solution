9e2a6c5efed0d19d10353238906d7060d1d9a0f0
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-07-15 16:14:10 +0600

    Create 915. Partition Array into Disjoint Intervals.java

---
+++ LeetCode Problems/901 to 1000/915. Partition Array into Disjoint Intervals.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int partitionDisjoint(int[] A) {
+        int N = A.length;
+        int[] maxleft = new int[N];
+        int[] minright = new int[N];
+
+        int m = A[0];
+        for (int i = 0; i < N; ++i) {
+            m = Math.max(m, A[i]);
+            maxleft[i] = m;
+        }
+
+        m = A[N-1];
+        for (int i = N-1; i >= 0; --i) {
+            m = Math.min(m, A[i]);
+            minright[i] = m;
+        }
+
+        for (int i = 1; i < N; ++i)
+            if (maxleft[i-1] <= minright[i])
+                return i;
+
+        throw null;
+    }
+}
