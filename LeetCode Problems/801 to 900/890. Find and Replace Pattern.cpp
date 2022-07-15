26c393f42480c7c85ce771265702ca5c35c577b6
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-07-15 16:14:07 +0600

    Create 890. Find and Replace Pattern.cpp

---
+++ LeetCode Problems/801 to 900/890. Find and Replace Pattern.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    vector<string> value;
+    string permutation(string s) {
+        string rep = "";
+        map<char, int> mp;
+        for (int i = 0; i < s.size(); i++) {
+            if (mp.find(s[i]) == mp.end()) {
+                mp.insert({s[i], i + 1});
+                rep += to_string(i + 1);
+            } else {
+                mp.insert({s[i], mp[s[i]]});
+                rep += to_string(mp[s[i]]);
+            }
+        }
+        return rep;
+    }
+public:
+    vector <string> findAndReplacePattern(vector <string> &words, string pattern) {
+        auto res = permutation(pattern);
+        for (auto it : words) {
+            if (res == permutation(it))
+                value.push_back(it);
+        }
+        return value;
+    }
+};
