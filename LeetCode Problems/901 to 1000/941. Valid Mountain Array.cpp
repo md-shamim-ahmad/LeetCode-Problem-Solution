5712bcc6f04d7a2c6f7da3e035736d71eb384567
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-07-18 06:54:17 +0600

    Create 941. Valid Mountain Array.cpp

---
+++ LeetCode Problems/901 to 1000/941. Valid Mountain Array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        bool check = false;
+        int i = 0, cntA = 0, cntB = 0;
+        for (i = 0; i < arr.size() - 1; i++) {
+            if (arr[i] < arr[i + 1])
+                cntA++;
+            else 
+                break;
+        }
+        for (; i < arr.size() - 1; i++) {
+            if (arr[i] > arr[i + 1])
+                cntB++;
+            else
+                break;
+        }
+        if (cntA != 0 && cntB != 0 && (cntA + cntB) == arr.size() - 1)
+            return true;
+        else 
+            return false;
+    }
+};
