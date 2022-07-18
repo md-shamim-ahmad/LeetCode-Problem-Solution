dcee73575af3339897260af56d6f14a29a0f5c89
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-07-18 06:53:57 +0600

    Create 496. Next Greater Element I.cpp

---
+++ LeetCode Problems/401 to 500/496. Next Greater Element I.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        for (int i = 0; i < nums1.size(); i++) {
+            int pos = -1;
+            for (int j = 0; j < nums2.size(); j++) {
+                if (nums1[i] == nums2[j]) {
+                    pos = j;
+                    break;
+                }
+            }
+            int max = -1;
+            for (int j = pos + 1; j < nums2.size(); j++) {
+                if (nums2[j] > nums1[i])  {
+                    max = nums2[j];
+                    break;
+                }
+            }
+            ans.push_back(max);
+        }
+        return ans;
+    }
+};
