34c299bf4aeb5a25966ad1045e881aa0cec64631
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-07-18 06:54:24 +0600

    Create 922. Sort Array By Parity II.cpp

---
+++ LeetCode Problems/901 to 1000/922. Sort Array By Parity II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        vector<int> odd, even;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] % 2 != 0)
+                odd.push_back(nums[i]);
+            else 
+                even.push_back(nums[i]);
+        }
+        for (int i = 0, j = 0, k = 0; i < nums.size(); i++) {
+            if ((i+1) % 2 == 0) 
+                nums[i] = odd[j++];
+            else 
+                nums[i] = even[k++];
+        }
+        return nums;
+    }
+};
