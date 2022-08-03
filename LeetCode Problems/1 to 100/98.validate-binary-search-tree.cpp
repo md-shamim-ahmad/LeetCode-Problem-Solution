91b5e604e88fd08949ba4bcbcb0831053b0ccadd
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-08-03 09:48:30 +0600

    Create 98.validate-binary-search-tree.cpp

---
+++ LeetCode Problems/1 to 100/98.validate-binary-search-tree.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=98 lang=cpp
+ *
+ * [98] Validate Binary Search Tree
+ */
+
+// @lc code=start
+
+  //Definition for a binary tree node.
+//   struct TreeNode {
+//       int val;
+//       TreeNode *left;
+//       TreeNode *right;
+//       TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//       TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//   };
+typedef long long ll;
+class Solution {
+    bool ans = false;
+public:
+    bool check (TreeNode *root, ll mn, ll mx) {
+        if (root == nullptr) return true;
+        int value = root->val;
+        if (value < mx && value > mn) {
+            return check(root->left, mn, root->val) and check(root->right, root->val, mx);
+        }        
+        return false;
+    }
+    bool isValidBST(TreeNode* root) {        
+        return check(root, LONG_MIN, LONG_MAX);
+    }
+};
+// @lc code=end
+
