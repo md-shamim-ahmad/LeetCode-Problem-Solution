792d766ea534920c2fee27008e7ea6bac628304d
Md-Shamim-Ahmmed <shamim.pust.cse11@gmail.com>
2022-08-21 16:41:26 +0600

    Unique Morse Code Words.cpp

---
+++ LeetCode Problems/801 to 900/804. Unique Morse Code Words.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        vector<string> key {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        map<string, int> mp;
+        for (int i = 0; i < words.size(); i++) {
+            string s = words[i], generate_str = "";
+            for (int j = 0; j < s.size(); j++) {
+                generate_str += key[s[j] - 'a'];
+            }
+            mp[generate_str]++;
+        }
+            return mp.size();
+    }
+};
