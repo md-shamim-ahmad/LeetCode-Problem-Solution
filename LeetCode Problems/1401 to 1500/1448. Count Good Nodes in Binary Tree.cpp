1a47b86aa89791f81059695a84889540df6cfc75
Md-Shamim-Ahmmed <shamim.pust.cse11@gmail.com>
2022-09-01 08:57:11 +0600

    Count Good Nodes in Binary Tree.cpp

---
+++ LeetCode Problems/1401 to 1500/1448. Count Good Nodes in Binary Tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int help(TreeNode* root, int value) {
+        if (root != nullptr) {
+            int cnt = help(root->left, max(value, root->val)) + help(root->right, max(value, root->val));
+            if (root->val >= value)
+                cnt++;
+            return cnt;
+        } 
+        return 0;
+    }
+    
+    int goodNodes(TreeNode* root) {
+        return help(root, INT_MIN);
+    }
+};
