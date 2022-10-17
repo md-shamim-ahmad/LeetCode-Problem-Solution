940c5499e0e6c65d94298157e1b8a9f0666ce304
Md-Shamim-Ahmmed <shamim.pust.cse11@gmail.com>
2022-10-17 07:59:15 +0600

    LeetCode Daily Challenge

---
+++ LeetCode Problems/1801 to 1900/1832. Check if the Sentence Is Pangram.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        vector<int> alphabet(26);
+        for (int i = 0; i < sentence.size(); i++) {
+            alphabet[sentence[i] - 'a']++;
+        }
+        for (auto it : alphabet) {
+            if (it == 0)
+                return false;
+        }
+        return true;
+    }
+};
