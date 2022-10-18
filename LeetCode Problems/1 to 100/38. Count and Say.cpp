22a9fd4eca6d2f129e0f692ca15e99ed373847d7
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-10-18 09:13:14 +0600

    LeetCode Daily Challenge, October-2022
    Recursive Solution

---
+++ LeetCode Problems/1 to 100/38. Count and Say.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n == 1)
+            return "1";
+        else {
+            string s = countAndSay(n - 1), generate_str="";            
+            int cnt = 1, i = 0;
+            for (i = 1; i < s.size(); i++) {                
+                if(s[i] != s[i - 1]) {
+                    generate_str += to_string(cnt) + s[i-1];
+                    cnt = 1;
+                }
+                else 
+                    cnt++;
+            }
+            if(s[i] != s[i - 1]) {
+                generate_str += to_string(cnt) + s[i-1];
+                cnt = 1;
+            }
+            return generate_str;
+        }
+    }
+};
