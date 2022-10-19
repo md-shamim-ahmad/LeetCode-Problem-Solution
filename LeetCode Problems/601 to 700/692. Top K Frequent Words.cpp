adb375b2296e5bd9e0c00f78f7e69406863486c3
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-10-19 16:17:13 +0600

    LeetCode Daily Challenge, 19-October-2022

---
+++ LeetCode Problems/601 to 700/692. Top K Frequent Words.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        map<string, int> mp;
+        for (auto ele : words) {
+            mp[ele]++;            
+        }
+        vector<pair<string, int>> frequent;
+        for (auto it : mp) {
+            frequent.push_back({it.first, it.second});            
+        }
+        sort(frequent.begin(), frequent.end(), [](auto a, auto b){
+            return a.first < b.first;
+        });
+        sort(frequent.begin(), frequent.end(), [](auto a, auto b){
+            if (a.second == b.second)
+                return a.first < b.first;
+            return a.second > b.second;
+        });        
+        vector<string> ans;
+        for (int i = 0; i < k; i++) 
+            ans.push_back(frequent[i].first);
+        return ans;
+    }
+};
