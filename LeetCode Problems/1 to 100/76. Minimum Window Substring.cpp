c54d48de807e7081987e48f232f39e5fae59b6ce
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-10-22 09:18:50 +0600

    LeetCode Daily Challenge, 22-October-2022

---
+++ LeetCode Problems/1 to 100/76. Minimum Window Substring.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        map<char, int> mp;
+        for (int i = 0; i < t.size(); i++) {
+            mp[t[i]]++;
+        }
+        int left = 0, right = 0, t_length = t.size(), min_length = 1e7, start=  0;
+        while(right < s.size()) {
+            if(--mp[s[right++]] >= 0)
+                t_length--;            
+            while(t_length == 0) {
+                if(right - left < min_length) {
+                    min_length = right - left;
+                    start = left;
+                }
+                if(++mp[s[left++]] > 0) t_length++;
+            }            
+        }        
+        return min_length < 1e7 ? s.substr(start, min_length) : "";
+    }
+};
