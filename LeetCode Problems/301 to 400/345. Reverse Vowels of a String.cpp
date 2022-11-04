72fe8fd9a544fab4a21e2763a73f96dc34ab56a6
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-11-04 08:24:53 +0600

    LeetCode Daily Challenge, 4-November-2022

---
+++ LeetCode Problems/301 to 400/345. Reverse Vowels of a String.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isVowel(char ch) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            return true;
+        if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            return true;
+        return false;
+    }        
+    string reverseVowels(string s) {
+        int l = 0, r = s.size() - 1;
+        while(l < r) {
+            if(isVowel(s[l]) && isVowel(s[r])) {
+                swap(s[l], s[r]);
+                l++, r--;
+            }
+            if (!isVowel(s[l]))
+                l++;
+            if (!isVowel(s[r]))
+                r--;
+        }
+        return s;
+    }
+};
