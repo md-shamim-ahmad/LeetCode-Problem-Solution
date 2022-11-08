cb9fe6f0f2dca59cfaa4d78ef9d2f0efe43b9616
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-11-08 08:33:22 +0600

    1544. Make The String Great

---
+++ LeetCode Problems/1501 to 1600/1544. Make The String Great/1544. Make The String Great.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> stc;
+        for (int i = 0; i < s.size(); i++) {
+            if(stc.empty()){
+                stc.push(s[i]);
+            }
+            else {
+                if((stc.top() == tolower(s[i]) || stc.top() == toupper(s[i])) && stc.top() != s[i]) {
+                    stc.pop();
+                } else {
+                    stc.push(s[i]);
+                }
+            }
+        }
+        s = "";
+        while(!stc.empty()) {
+            s += stc.top();
+            stc.pop();
+        }
+        reverse(s.begin(), s.end());
+        return s;
+    }
+};
---
+++ LeetCode Problems/1501 to 1600/1544. Make The String Great/1544. Make The String Great.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String makeGood(String s) {
+        StringBuffer str = new StringBuffer(s);
+        int i = 0;
+        while (i < str.length() - 1) {
+            if(Character.toLowerCase(str.charAt(i)) == Character.toLowerCase(str.charAt(i + 1))
+                && (Character.isUpperCase(str.charAt(i)) == Character.isLowerCase(str.charAt(i + 1)) )) {
+                str.deleteCharAt(i);
+                str.deleteCharAt(i);
+                if (i > 0)
+                    i--;
+                else i = 0;
+                continue;
+            }
+            i+=1;
+        }
+        return str.toString();
+    }
+}
