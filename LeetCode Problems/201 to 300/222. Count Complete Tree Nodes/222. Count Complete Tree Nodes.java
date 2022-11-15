cf86332fead575538c50ae8e27c4c7230fa35ff4
ahmmed-sde <117422424+ahmmed-sde@users.noreply.github.com>
2022-11-15 07:18:12 +0600

    222. Count Complete Tree Nodes (#9)
    * update 222. Count Complete Tree Nodes.cpp
    
    * Create 222. Count Complete Tree Nodes.java

---
+++ LeetCode Problems/201 to 300/222. Count Complete Tree Nodes/222. Count Complete Tree Nodes.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int cnt = 0;
+    public int countNodes(TreeNode root) {
+        if(root == null)
+            return 0;
+        if(root.left != null)
+            countNodes(root.left);
+        cnt += 1;
+        if(root.right != null)
+            countNodes(root.right);        
+        return cnt;
+    }
+}
