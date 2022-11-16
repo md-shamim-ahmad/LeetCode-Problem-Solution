d3c6a9dc8ab290fcebde86126536c5d365f7656a
ahmmed-sde <117422424+ahmmed-sde@users.noreply.github.com>
2022-11-16 09:54:26 +0600

    Create  66. Plus One.java

---
+++ LeetCode Problems/1 to 100/66. Plus One/66. Plus One.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int i = digits.length - 1; i >= 0; i--) {
+            if(digits[i] == 9) {
+                if(i > 0) {
+                    digits[i] = 0;
+                    if(digits[i - 1] != 9) {
+                        digits[i -1] += 1; break;
+                    }
+                    continue;
+                }
+                digits[i] = 0;
+                int []digit = new int[digits.length + 1];
+                System.arraycopy(digits, 0, digit, 0, 0);
+                digit[0] = 1;
+                return digit;
+            }
+            digits[i] += 1; break;            
+        }
+        return digits;
+    }
+}
