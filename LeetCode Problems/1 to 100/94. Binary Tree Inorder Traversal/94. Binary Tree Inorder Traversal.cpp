0de9996720dd477cbd13f5eb49fe252a7397dbc4
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-11-21 18:26:57 +0600

    94. Binary Tree Inorder Traversal

---
+++ LeetCode Problems/1 to 100/94. Binary Tree Inorder Traversal/94. Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    vector<int> arr;
+    void inorder(TreeNode* root){
+        if(root == nullptr) return;
+        inorder(root->left);
+        arr.push_back(root->val);
+        inorder(root->right);
+    }
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        inorder(root);
+        return arr;
+    }
+};
---
+++ LeetCode Problems/1 to 100/94. Binary Tree Inorder Traversal/94. Binary Tree Inorder Traversal.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private List<Integer> list = new ArrayList<>();
+    private void inorder(TreeNode root) {
+        if(root == null) return;
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        inorder(root);
+        return list;
+    }
+}
