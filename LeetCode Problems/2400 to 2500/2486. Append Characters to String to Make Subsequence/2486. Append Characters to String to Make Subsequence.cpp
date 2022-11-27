0f6df0fc0820e22f31c82347429feda0755a1dce
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-11-27 10:28:45 +0600

    Weekly Contest 321

---
+++ LeetCode Problems/2400 to 2500/2486. Append Characters to String to Make Subsequence/2486. Append Characters to String to Make Subsequence.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int j = 0;
+        for(int i = 0; i < s.size() && j < t.size(); i++) {
+            if(s[i] == t[j])
+                j++;
+        }
+        return t.size() - j;
+    }
+};
---
+++ LeetCode Problems/2400 to 2500/2487. Remove Nodes From Linked List/2487. Remove Nodes From Linked List.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    ListNode* reverse_it(ListNode* head){
+        ListNode *prev = NULL;
+        ListNode *curr = head, *next;
+        while(curr!=NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+public:
+    
+    ListNode* removeNodes(ListNode* head) {
+        head  = reverse_it(head);
+        int mx = (head)->val;
+        ListNode* prev = head;
+         ListNode* curr = (head)->next;
+        while(curr != NULL){
+            if(curr->val < mx){
+                ListNode* temp = curr;
+                prev->next = curr->next;
+                curr = curr->next;
+                delete temp;
+            } else {
+                mx = max(mx, curr->val);
+                prev = curr;
+                curr = curr->next;
+            }
+        }
+        head  = reverse_it(head);
+        return head;
+    }
+};
