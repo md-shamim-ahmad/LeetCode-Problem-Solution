b3135b5fc08cf5432b8ca09029d54c6ec897b380
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-11-28 21:05:00 +0600

    Find Players With Zero or One Losses.cpp

---
+++ LeetCode Problems/2201 to 2300/2225. Find Players With Zero or One Losses/2225. Find Players With Zero or One Losses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        map<int, int> lose, wine;
+        for(int i = 0; i < matches.size(); i++) {
+            wine[matches[i][0]]++, lose[matches[i][1]]++;
+        }
+        vector<vector<int>> ans(2);
+        for(auto it : wine) {
+            if(lose[it.first] == 0) 
+                ans[0].push_back(it.first);
+        }
+        for(auto it : lose) {
+            if(it.second == 1)
+                ans[1].push_back(it.first);
+        }
+        return ans;
+    }
+};
