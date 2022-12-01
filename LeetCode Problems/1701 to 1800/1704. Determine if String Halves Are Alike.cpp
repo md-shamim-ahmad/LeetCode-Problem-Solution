1a1e46d780b06607027900346460bdf85a08693f
ahmmed-sde <117422424+ahmmed-sde@users.noreply.github.com>
2022-12-01 08:42:22 +0600

    Update 1704. Determine if String Halves Are Alike.cpp

--- LeetCode Problems/1701 to 1800/1704. Determine if String Halves Are Alike.cpp
+++ LeetCode Problems/1701 to 1800/1704. Determine if String Halves Are Alike.cpp
@@ -1,22 +1,16 @@
 class Solution {
+    inline bool isVowel(char ch) {
+        ch = tolower(ch);
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') return true;
+        return false;
+    }
 public:
     bool halvesAreAlike(string s) {
-        string a = s.substr(0, s.size() / 2);
-        string b = s.substr(s.size() / 2, s.size());
-        int cnt = 0;
-        for (auto ele : a){
-            char ch = tolower(ele);
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-                cnt++;
+        int l = s.size() / 2, cnt_half = 0, cnt_another_half = 0;
+        for (int i = 0, j = l; i < l && j < s.size(); i++, j++) {
+            if(isVowel(s[i])) cnt_half++;
+            if(isVowel(s[j])) cnt_another_half++;
         }
-        for (auto ele : b){
-            char ch = tolower(ele);
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-                cnt--;
-        }
-        if (cnt == 0)
-            return true;
-        else
-            return false;
+        return cnt_another_half == cnt_half;
     }
 };
