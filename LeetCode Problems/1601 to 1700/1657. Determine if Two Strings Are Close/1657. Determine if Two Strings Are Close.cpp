d276d22001d9b31ef05f64f2b28779fefe9536fd
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-12-02 16:50:11 +0600

    Determine if Two Strings Are Close.cpp

---
+++ LeetCode Problems/1601 to 1700/1657. Determine if Two Strings Are Close/1657. Determine if Two Strings Are Close.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        vector<int>val1(26), val2(26);
+        set<char>s1, s2;
+        for(auto it :  word1){
+            val1[it - 'a']++;
+            s1.insert(it);
+        }
+        for(auto it : word2){
+            val2[it - 'a']++;
+            s2.insert(it);
+        }
+        sort(val1.begin(), val1.end()), sort(val2.begin(), val2.end());
+        return val1 == val2 && s1 == s2;
+    }
+};
