d904eeece3bc4acf893319643d10e131558720ab
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-12-07 10:45:18 +0600

    938. Range Sum of BST

---
+++ LeetCode Problems/901 to 1000/938. Range Sum of BST/938. Range Sum of BST.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int ans = 0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root == null)
+            return 0;
+        if(root.val >= low && root.val <= high)
+            ans  += root.val;
+        rangeSumBST(root.left, low, high);
+        rangeSumBST(root.right, low, high);
+        return ans;
+    }
+}
