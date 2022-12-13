683a8f1e968af42fd5c0ae4afe82b78e520438d1
ahmmed-sde <117422424+ahmmed-sde@users.noreply.github.com>
2022-12-14 00:25:10 +0600

    Linked List Cycle.java

---
+++ LeetCode Problems/101 to 200/141. Linked List Cycle/141. Linked List Cycle.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+    }
+}
