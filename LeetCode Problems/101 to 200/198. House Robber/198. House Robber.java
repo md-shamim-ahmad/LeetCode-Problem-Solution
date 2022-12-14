c1aacd40a81438684ba6a1b59ed0f74281bf4495
Md-Shamim-Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-12-14 11:20:17 +0600

    House Robber.java

---
+++ LeetCode Problems/101 to 200/198. House Robber/198. House Robber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n <= 1)
+            return nums[0];        
+        int prev = nums[0], curr = Math.max(nums[1], nums[0]);
+        for (int i = 2; i < n; i++) {
+            int temp = Math.max(prev + nums[i], curr);
+            prev = curr;
+            curr = temp;
+        }
+        return curr;
+    }
+}
