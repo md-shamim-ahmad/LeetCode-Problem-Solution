5618ade778180f1dcdd8c880e58c2067009ab762
Md-Shamim-Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-12-14 11:20:11 +0600

    House Robber.cpp

---
+++ LeetCode Problems/101 to 200/198. House Robber/198. House Robber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 1)
+            return nums[0];        
+        int prev = nums[0], curr = max(nums[1], nums[0]);
+        for (int i = 2; i < n; i++) {
+            int temp = max(prev + nums[i], curr);
+            prev = curr;
+            curr = temp;
+        }
+        return curr;
+    }
+};
