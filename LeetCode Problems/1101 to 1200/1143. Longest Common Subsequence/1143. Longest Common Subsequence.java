dd0a22009263348425ccf131bc25e86737746c53
Md-Shamim-Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-12-16 00:16:04 +0600

    Longest Common Subsequence.java

---
+++ LeetCode Problems/1101 to 1200/1143. Longest Common Subsequence/1143. Longest Common Subsequence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int dp[][] = new int[text1.length() + 1][text2.length() + 1];
+        for(int i = 0; i < text1.length(); i++) {
+            for (int j = 0; j < text2.length(); j++) {
+                if(text1.charAt(i) == text2.charAt(j))
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                else 
+                    dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+            }
+        }
+        return dp[text1.length()][text2.length()];
+    }
+}
