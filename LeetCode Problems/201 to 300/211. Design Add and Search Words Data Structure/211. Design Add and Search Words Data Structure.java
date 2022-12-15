c7d904d789847a90744fe6e178aba92efd0e1e94
Md-Shamim-Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-12-15 11:22:44 +0600

    Design Add and Search Words Data Structure.java

---
+++ LeetCode Problems/201 to 300/211. Design Add and Search Words Data Structure/211. Design Add and Search Words Data Structure.java
@@ -0,0 +1,21 @@
+class WordDictionary {
+
+    public WordDictionary() {
+        
+    }
+    
+    public void addWord(String word) {
+        
+    }
+    
+    public boolean search(String word) {
+        
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
