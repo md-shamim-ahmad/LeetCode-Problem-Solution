676372c00d576bf78a975a72d5f1d481d87db2cb
Md-Shamim-Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2022-12-27 10:11:30 +0600

    Jump Game.java

---
+++ LeetCode Problems/1 to 100/55. Jump Game/55. Jump Game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {        
+        int n = nums.length;
+        int indx = n - 1;        
+	for(int i = n - 2; i >= 0; i--) {
+		if(i + nums[i] >= indx)
+                indx = i;
+	}
+	return indx == 0;
+    }
+}
+
