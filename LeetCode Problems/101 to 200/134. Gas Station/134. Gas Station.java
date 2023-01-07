145d8cf2fd2c4a309e28399b7ece1447e7f38303
Md-Shamim-Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-01-07 09:24:46 +0600

    Daily LeetCoding Challenge January-2023, Day 7
    134. Gas Station

---
+++ LeetCode Problems/101 to 200/134. Gas Station/134. Gas Station.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total_gas = 0, total_cost = 0;
+        for(int i = 0; i < gas.length; i++) {
+            total_gas += gas[i];
+            total_cost += cost[i];
+        }
+        if(total_cost > total_gas)
+            return -1;
+        int startStation = 0, current_gas = 0;
+        for(int i = 0; i < gas.length; i++) {
+            if(current_gas < 0) {
+                current_gas = 0;
+                startStation = i;
+            }
+            current_gas += (gas[i] - cost[i]);
+        }
+        return startStation;
+    }
+}
