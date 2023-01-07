57812585f7445d8b90cc81a034f787f04ce86a51
ahmmed-sde <117422424+ahmmed-sde@users.noreply.github.com>
2023-01-07 09:42:39 +0600

    Update 134. Gas Station.cpp

--- LeetCode Problems/101 to 200/134. Gas Station/134. Gas Station.cpp
+++ LeetCode Problems/101 to 200/134. Gas Station/134. Gas Station.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int total_gas = 0, total_cost = 0;
-        for(int i = 0; i < gas.size(); i++) {
-            total_gas += gas[i];
-            total_cost += cost[i];
-        }
-        if(total_cost > total_gas)
-            return -1;
-        int startStation = 0, current_gas = 0;
+        int total = 0, startStation = 0, current_gas = 0;
         for(int i = 0; i < gas.size(); i++) {
             if(current_gas < 0) {
                 current_gas = 0;
                 startStation = i;
             }
             current_gas += (gas[i] - cost[i]);
+            total += (gas[i] - cost[i]);
         }
-        return startStation;
+        return (total < 0 ? -1 : startStation);
     }
 };
