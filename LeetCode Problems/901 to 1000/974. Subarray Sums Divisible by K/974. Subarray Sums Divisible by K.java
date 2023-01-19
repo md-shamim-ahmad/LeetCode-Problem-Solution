808557b04f010e0bd7cd81cba714d6eb676ff3ba
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-01-20 01:23:53 +0600

    Subarray Sums Divisible by K.java

---
+++ LeetCode Problems/901 to 1000/974. Subarray Sums Divisible by K/974. Subarray Sums Divisible by K.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int sum = 0, cnt = 0, rem;
+        int []prefix = new int[k];
+        prefix[0]++;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            rem = (sum % k + k)% k;
+            cnt += prefix[rem];
+            prefix[rem]++;
+        }
+        return cnt;
+    }
+}
