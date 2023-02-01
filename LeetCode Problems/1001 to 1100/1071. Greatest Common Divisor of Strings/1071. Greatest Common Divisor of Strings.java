f5712da7b4739e5ea15488d32d7506ebc8dc6032
ahmmed-sde <117422424+ahmmed-sde@users.noreply.github.com>
2023-02-01 19:18:33 +0600

    Greatest Common Divisor of Strings.java

---
+++ LeetCode Problems/1001 to 1100/1071. Greatest Common Divisor of Strings/1071. Greatest Common Divisor of Strings.java
@@ -0,0 +1,13 @@
+class Solution {
+    private int gcd(int a, int b) {
+        if(b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+    public String gcdOfStrings(String str1, String str2) {
+        if((str1+str2).equals(str2+str1))
+            return str1.substring(0, gcd(str1.length(), str2.length()));
+        else 
+            return "";
+    }
+}
