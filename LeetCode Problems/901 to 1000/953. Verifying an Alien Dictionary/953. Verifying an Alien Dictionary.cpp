adeb98b86a10763548ab8e15c28f2aa5cc7a2fa2
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-02-02 08:05:31 +0600

    Create 953. Verifying an Alien Dictionary.cpp

---
+++ LeetCode Problems/901 to 1000/953. Verifying an Alien Dictionary/953. Verifying an Alien Dictionary.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        map<char, int> mp;
+        for (int i = 0; i < order.size(); i++)
+            mp[order[i]] = i;
+        vector<pair<string, int>> list;
+        int k = 0;
+        for(auto s : words) {
+            string str = "";
+            for (int i = 0; i < s.size(); i++) {
+                str += ('a' + mp[s[i]]);
+            }
+            list.emplace_back(str, k++);
+        }
+        sort(list.begin(), list.end());
+        for (int i = 0; i < list.size(); i++) {
+            if(i != list[i].second)
+                return false;
+        }
+        return true;
+    }
+};
