90b62fb074f99f2a106c03702d8bb57a02826de8
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-02-14 12:39:49 +0600

    Add Binary.cpp

---
+++ LeetCode Problems/1 to 100/67. Add Binary/67. Add Binary.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result;
+        int carry = 0;
+        for (int i = a.size() - 1, j = b.size() - 1; i >= 0 || j >= 0 || carry; --i, --j) {
+            carry += i >= 0 ? a[i] - '0' : 0;
+            carry += j >= 0 ? b[j] - '0' : 0;
+            result += (carry & 1) + '0';
+            carry >>= 1;
+        }
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
+};
