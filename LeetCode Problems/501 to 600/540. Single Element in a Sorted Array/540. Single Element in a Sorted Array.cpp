b5b14d14ab49b5d2697bfdcfcda577642081ecbe
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-02-21 22:52:25 +0600

    Single Element in a Sorted Array.cpp

---
+++ LeetCode Problems/501 to 600/540. Single Element in a Sorted Array/540. Single Element in a Sorted Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+        while(l < r) {
+            int m = (l + r) / 2;
+            if (m % 2 == 1) 
+                m--;
+            if (nums[m] != nums[m + 1])
+                r = m;
+            else 
+                l = m + 2;
+        }
+        return nums[l];
+    }
+};
