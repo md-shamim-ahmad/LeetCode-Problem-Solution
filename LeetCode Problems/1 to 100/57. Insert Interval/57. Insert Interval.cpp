e717f5aff41c2baf1690aae871de64f56206c8a7
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-02-21 23:07:13 +0600

    Update 57. Insert Interval.cpp

--- LeetCode Problems/1 to 100/57. Insert Interval/57. Insert Interval.cpp
+++ LeetCode Problems/1 to 100/57. Insert Interval/57. Insert Interval.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        
+        vector<vector<int>>ans;
+        for(auto &&i:intervals){
+            if(i[1]<newInterval[0])ans.push_back(i);
+            else if(newInterval[1]<i[0]){
+                ans.push_back(newInterval);
+                newInterval=i;
+            }
+            else{
+                newInterval[0]=min(newInterval[0],i[0]);
+                newInterval[1]=max(newInterval[1],i[1]);
+            }    
+        }
+        ans.push_back(newInterval);
+        return ans;
     }
 };
