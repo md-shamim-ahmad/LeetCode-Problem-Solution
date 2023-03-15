eb046e285e82f2d6dec0df8d774bcc2476e75748
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-03-15 09:03:59 +0600

    Check Completeness of a Binary Tree.java

---
+++ LeetCode Problems/901 to 1000/958. Check Completeness of a Binary Tree/958. Check Completeness of a Binary Tree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+    private boolean checker(TreeNode root, int currNode, int totalNode) {
+        if (root == null) return true;
+        if(currNode >= totalNode) return false;
+        return checker(root.left, 2 * currNode + 1, totalNode) 
+                                    &&
+                checker(root.right, 2 * currNode + 2, totalNode);
+    }
+    public boolean isCompleteTree(TreeNode root) {
+        return checker(root, 0, countNodes(root));
+    }
+}
