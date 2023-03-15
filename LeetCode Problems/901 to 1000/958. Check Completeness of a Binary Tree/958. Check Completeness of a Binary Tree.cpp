72c557e1948c0b9cc1fa5cf3bb5c4909f4d2c7cf
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-03-15 09:04:07 +0600

    Check Completeness of a Binary Tree.cpp

---
+++ LeetCode Problems/901 to 1000/958. Check Completeness of a Binary Tree/958. Check Completeness of a Binary Tree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        return checker(root, 0, countNodes(root));
+    }
+private:
+    int countNodes(TreeNode* root) {
+        if (root == NULL) return 0;
+        return countNodes(root->left) + countNodes(root->right) + 1;
+    }
+    bool checker(TreeNode* root, int currNode, int totalNode) {
+        if (root == NULL) return true;
+        if(currNode >= totalNode) return false;
+        return checker(root->left, 2 * currNode + 1, totalNode) 
+                                    &&
+                checker(root->right, 2 * currNode + 2, totalNode);
+    }
+};
