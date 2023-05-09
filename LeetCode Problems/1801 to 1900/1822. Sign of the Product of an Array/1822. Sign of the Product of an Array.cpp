24b8e5c31818eb97c228b20ead86612e4a9a4f75
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-05-09 07:53:50 +0600

    Sign of the Product of an Array.cpp

---
+++ LeetCode Problems/1801 to 1900/1822. Sign of the Product of an Array/1822. Sign of the Product of an Array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int sign = 0;
+        for (auto it : nums) {
+            if(it == 0) return 0;
+            if (it < 0) sign++;
+        }
+        return sign % 2 == 0 ? 1 : -1;
+    }
+};
