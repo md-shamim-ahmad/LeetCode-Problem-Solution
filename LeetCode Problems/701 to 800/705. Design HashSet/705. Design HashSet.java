a46169327793599f7a17d6a273b7cc461350abc3
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-05-30 09:10:36 +0600

    Design HashSet.java

---
+++ LeetCode Problems/701 to 800/705. Design HashSet/705. Design HashSet.java
@@ -0,0 +1,31 @@
+class MyHashSet {
+    ArrayList<Integer> list;
+    public MyHashSet() {
+        list = new ArrayList<>();
+    }
+
+    int getIndex(int key) {
+        for (int i = 0; i < list.size(); i++)
+            if(list.get(i) == key)
+                return i;
+        return -1;
+    }
+    public void add(int key) {
+        list.add(key);
+    }
+
+    public void remove(int key) {
+        for (int i = 0; i < list.size(); i++)
+            if(list.get(i) == key) {
+                list.remove(i);
+                i--;
+            }
+    }
+
+    public boolean contains(int key) {
+        int pos = getIndex(key);
+        if(pos >= 0)
+            return list.get(pos) == key;
+        return false;
+    }
+}
