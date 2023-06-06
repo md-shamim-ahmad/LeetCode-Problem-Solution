4618306cc1f47459289060c3ff2be8dcb08586f1
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-06-06 07:06:53 +0600

    Can Make Arithmetic Progression From Sequence.java

---
+++ LeetCode Problems/1501 to 1600/1502. Can Make Arithmetic Progression From Sequence/1502. Can Make Arithmetic Progression From Sequence.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        int temp = arr[1] - arr[0];
+        for (int i = 1; i < arr.length - 1; i++) {
+            if(arr[i + 1] - arr[i] != temp)
+                return false;
+        }
+        return true;
+    }
+}
