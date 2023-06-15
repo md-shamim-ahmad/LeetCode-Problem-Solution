2cd3c8398f59aeadb905ad5099472f6b38871884
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-06-15 06:01:07 +0600

    Equal Row and Column Pairs.cpp

---
+++ LeetCode Problems/2301 to 2400/2352. Equal Row and Column Pairs/2352. Equal Row and Column Pairs.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        vector<string> arr, a;
+        for (int i = 0; i < grid.size(); i++) {
+            string s = "";
+            for (int j = 0; j < grid.size(); j++) {
+                s += to_string(grid[i][j]) + " ";
+            }
+            arr.push_back(s);
+            s = "";
+            for (int j = 0; j < grid.size(); j++) {
+                s += to_string(grid[j][i]) + " ";
+            }
+            a.push_back(s);
+        }
+        int cnt = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = 0; j < a.size(); j++) {
+                if (arr[i] == a[j])
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+};
