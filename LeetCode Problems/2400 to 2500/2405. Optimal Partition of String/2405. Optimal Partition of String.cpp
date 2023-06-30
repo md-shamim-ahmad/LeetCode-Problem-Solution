5b5856af8c28b438026e981016cde907f8ca9c1e
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-06-30 07:06:10 +0600

    Optimal Partition of String.cpp

---
+++ LeetCode Problems/2400 to 2500/2405. Optimal Partition of String/2405. Optimal Partition of String.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int partitionString(string s) {
+        map<char, int> mp;
+        int ans = 1;
+        for(char ch : s) {            
+            if (mp[ch] >= 1) {
+                ans++;
+                mp.clear();
+            }
+            mp[ch]++;
+        }
+        return ans;
+    }
+};
