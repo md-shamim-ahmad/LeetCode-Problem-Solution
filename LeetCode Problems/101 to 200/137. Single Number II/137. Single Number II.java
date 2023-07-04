812593e835cab74224a3f439504ddbe0346b3662
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-07-04 07:39:24 +0600

    Single Number II.java

---
+++ LeetCode Problems/101 to 200/137. Single Number II/137. Single Number II.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int ele : nums) {
+            map.merge(ele, 1, Integer::sum);
+        }
+        int ans = -1;
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(entry.getValue() == 1)
+                ans = entry.getKey();
+        }
+        return ans;
+    }
+}
