88adef0ec75b6d9f10a0bb54186dc26a70f42a27
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-07-04 07:44:15 +0600

    Single Number II.cpp

---
+++ LeetCode Problems/101 to 200/137. Single Number II/137. Single Number II.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int, int> mp;
+        for(auto ele : nums) {
+            mp[ele]++;
+        }        
+        for(auto it : mp) {
+            if(it.second == 1)
+                return it.first;
+        }
+        return 0;
+    }
+};
