7267a10e9c4bde09fbe0bfb658ae52c1a14893d4
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-07-09 21:53:17 +0600

    Partition Labels.cpp

---
+++ LeetCode Problems/701 to 800/763. Partition Labels/763. Partition Labels.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int map[26];
+        for (int i = 0; i < s.size(); i++) {
+            map[s[i] - 'a'] = i+1;
+        }
+        int size = 0, r = 0;
+        vector<int> arr;
+        for (int l = 0; l < s.size(); l++) {
+            if(l == r && l != 0) {
+                arr.push_back(size);
+                size = 0;
+            }
+            if(l <= r) {
+                r = max(r, map[s[l] - 'a']);
+                size++;
+            }
+        }
+        arr.push_back(size);
+        return arr;
+    }
+};
