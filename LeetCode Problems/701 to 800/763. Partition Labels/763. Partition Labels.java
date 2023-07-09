c478ff0f1f0fdc3924143d2e2633e45b84fcc881
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-07-09 21:53:24 +0600

    Partition Labels.java

---
+++ LeetCode Problems/701 to 800/763. Partition Labels/763. Partition Labels.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int []map = new int[27];
+        for(int i = 0; i < s.length(); i++) {
+            map[s.charAt(i) - 'a'] = i + 1;
+        }
+        int size = 0, r = 0;
+        List<Integer> arr = new ArrayList<>();
+        for(int l = 0; l < s.length(); l++) {
+            if(l == r && l != 0) {
+                arr.add(size);
+                size = 0;
+            }
+            if (l <= r) {
+                r = Math.max(r, map[s.charAt(l) - 'a']);
+                size++;
+            }
+        }
+        arr.add(size);
+        return arr;
+    }
+}
