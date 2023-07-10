a1a9a98dfdb0bcbd71e809a82776bed9cc8d2e73
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-07-10 14:40:15 +0600

    Minimum Add to Make Parentheses Valid.java

---
+++ LeetCode Problems/901 to 1000/921. Minimum Add to Make Parentheses Valid/921. Minimum Add to Make Parentheses Valid.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int cnt = 0;
+        String str = "";
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') 
+                str += s.charAt(i);
+            else {
+                if(s.charAt(i) == ')' && str.length() > 0) {
+                    StringBuilder sb = new StringBuilder(str);
+                    sb.deleteCharAt(0);
+                    str = sb.toString();
+                }                
+                else
+                    cnt++;
+            }
+        }
+        return cnt + str.length();
+    }
+}
