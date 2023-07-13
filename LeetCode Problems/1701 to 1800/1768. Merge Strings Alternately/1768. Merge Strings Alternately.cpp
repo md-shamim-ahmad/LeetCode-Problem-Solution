aea34e3c600fdb0fbaeb83edac092da82945573c
md-shamim-ahmmed <shamim.pust.cse11@gmail.com>
2023-07-13 11:18:23 +0600

    Merge Strings Alternately.cpp

---
+++ LeetCode Problems/1701 to 1800/1768. Merge Strings Alternately/1768. Merge Strings Alternately.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string str = "";
+        bool check = true;
+        int i = 0, j = 0;
+        for (; i < word1.size() && j < word2.size();) {
+            if (check) {
+                str += word1[i++];
+                check = false;
+            }else {
+                str += word2[j++];
+                check = true;
+            }
+        }
+        for(; i < word1.size(); i++)
+            str += word1[i];
+        for(; j < word2.size(); j++)
+            str += word2[j];
+
+        return str;
+    }
+};
