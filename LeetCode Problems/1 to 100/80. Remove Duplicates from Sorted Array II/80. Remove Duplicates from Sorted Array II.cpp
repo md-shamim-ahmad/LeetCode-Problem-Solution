de7b5787f02ebe0836c7db09778be7b763df3ead
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-07-15 18:56:10 +0600

    Remove Duplicates from Sorted Array II.cpp

---
+++ LeetCode Problems/1 to 100/80. Remove Duplicates from Sorted Array II/80. Remove Duplicates from Sorted Array II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        map<int, int> mp;
+        for(auto it : nums)
+            mp[it]++;
+        int sum = 0;
+        nums.clear();
+        for(auto it : mp) {
+            sum += min(2, it.second);
+            for(int i = 0; i < min(2, it.second); i++) {
+                nums.push_back(it.first);
+            }
+        }
+        return sum;
+    }
+};
