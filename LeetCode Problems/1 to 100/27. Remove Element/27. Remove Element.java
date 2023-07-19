95b31f6a7a7fa47ae3e20ec2080afdff08dd794d
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-07-19 23:15:24 +0600

    Remove Element.java

---
+++ LeetCode Problems/1 to 100/27. Remove Element/27. Remove Element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        List<Integer> list = new ArrayList<>();
+        for (int ele : nums) {
+            if(ele != val)
+                list.add(ele);
+        }
+        int n = list.size();
+        for(int i = 0; i < n; i++)
+            nums[i] = list.get(i);
+        return n;
+    }
+}
