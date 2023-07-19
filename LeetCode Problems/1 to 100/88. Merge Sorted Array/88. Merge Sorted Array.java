abf8dd7d67ea2d755858b3b83b4687f9b8bc1057
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-07-19 22:26:41 +0600

    Merge Sorted Array.java

---
+++ LeetCode Problems/1 to 100/88. Merge Sorted Array/88. Merge Sorted Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int num[] = new int[m+n];
+        int k = 0, i = 0, j = 0;
+        while(i < m && j < n) {
+            if(nums1[i] < nums2[j])
+                num[k++] = nums1[i++];
+            else
+                num[k++] = nums2[j++];
+        }
+        while(i < m)
+            num[k++] = nums1[i++];
+        while(j < n)
+            num[k++] = nums2[j++];
+        for (i = 0; i < n + m; i++)
+            nums1[i] = num[i];
+    }
+}
