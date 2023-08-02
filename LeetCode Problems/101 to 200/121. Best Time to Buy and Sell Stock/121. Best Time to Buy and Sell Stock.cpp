9e9efa3318314005f8e6de0588b764b857db30a7
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-08-02 23:21:54 +0600

    121. Best Time to Buy and Sell Stock

--- LeetCode Problems/101 to 200/121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.cpp
+++ LeetCode Problems/101 to 200/121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        
+        int l = 0, mx = 0, r = 1;
+        while(r < prices.size()) {
+            if (prices[r] < prices[l])
+                l = r;
+            else
+                mx = max(mx, prices[r] - prices[l]);
+            r += 1;
+        }
+        return mx;
     }
 };
---
+++ LeetCode Problems/101 to 200/121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int mx = 0, l = 0, r = 1;
+        while(r < prices.length) {
+            if (prices[r] <= prices[l])
+                l = r;
+            else
+                mx = Math.max(mx, prices[r] - prices[l]);
+            r++;
+        }
+        return mx;
+    }
+}
