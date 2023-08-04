caab573a0412036a858ece7819214c3cb4752bdc
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-08-04 20:02:27 +0600

    Word Break.java

---
+++ LeetCode Problems/101 to 200/139. Word Break/139. Word Break.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean []dp = new boolean[n + 1];
+        Arrays.fill(dp, false);
+        dp[n] = true;
+        for (int i = n - 1; i >= 0; i--) {
+            for (String w : wordDict) {
+                if (i + w.length() <= n && s.substring(i, i + w.length()).equals(w)) {
+                    dp[i] = dp[i + w.length()];
+                }
+                if (dp[i]) {
+                    break;
+                }
+            }
+        }
+        return dp[0];
+    }
+}
