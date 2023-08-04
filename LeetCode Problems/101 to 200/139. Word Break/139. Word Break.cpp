921083c3e521848d2b315a95ee5fc95ff3908017
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-08-04 20:02:13 +0600

    Update 139. Word Break.cpp

--- LeetCode Problems/101 to 200/139. Word Break/139. Word Break.cpp
+++ LeetCode Problems/101 to 200/139. Word Break/139. Word Break.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        
+        int n = s.length();
+        vector<bool> dp(n + 1, false);
+        dp[n] = true;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (const std::string& w : wordDict) {
+                if (i + w.length() <= n && s.substr(i, w.length()) == w) {
+                    dp[i] = dp[i + w.length()];
+                }
+                if (dp[i]) {
+                    break;
+                }
+            }
+        }
+
+        return dp[0];
     }
 };
