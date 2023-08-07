cb1715dec3854a23d1c1cca6d4c9210576949745
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-08-07 08:53:00 +0600

    Search a 2D Matrix.java

---
+++ LeetCode Problems/1 to 100/74. Search a 2D Matrix/74. Search a 2D Matrix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int l = 0, r = n * m - 1;
+        while(l <= r) {
+            int mid = r + (l - r) / 2;
+            int ele = matrix[mid / m][mid % m];
+            if(ele == target)
+                return true;
+            else if (ele < target)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return false;
+    }
+}
