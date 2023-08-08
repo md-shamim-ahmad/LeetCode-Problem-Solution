cab2f4f90f3bb75ba9e1d38f41bd821c437a58ec
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-08-08 20:17:09 +0600

    Search in Rotated Sorted Array.java

---
+++ LeetCode Problems/1 to 100/33. Search in Rotated Sorted Array/33. Search in Rotated Sorted Array.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++)
+            if (target == nums[i])
+                return i;
+        return -1;
+    }
+}
