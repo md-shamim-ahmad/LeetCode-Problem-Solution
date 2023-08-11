e726a928fe7d4f699372b5f95e1f600b37faaf17
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-08-11 11:19:17 +0600

    Update 70. Climbing Stairs.java

--- LeetCode Problems/1 to 100/70. Climbing Stairs/70. Climbing Stairs.java
+++ LeetCode Problems/1 to 100/70. Climbing Stairs/70. Climbing Stairs.java
@@ -1,12 +1,10 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n <= 1)
-            return 1;
         int dp[] = new int[n + 1];
-        dp[1] = 1; dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
-        }        
+        }
         return dp[n];
     }
 }
