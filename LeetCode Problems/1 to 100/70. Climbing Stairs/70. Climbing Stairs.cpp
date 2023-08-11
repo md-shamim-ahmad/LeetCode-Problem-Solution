fd047df86f5bcdf6ec72088668feda5d335504c5
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-08-11 11:19:13 +0600

    Update 70. Climbing Stairs.cpp

--- LeetCode Problems/1 to 100/70. Climbing Stairs/70. Climbing Stairs.cpp
+++ LeetCode Problems/1 to 100/70. Climbing Stairs/70. Climbing Stairs.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n + 3);
-        dp[1] = 1, dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
+        vector<int> dp(n + 1);
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
-        }        
+        }
         return dp[n];
     }
 };
