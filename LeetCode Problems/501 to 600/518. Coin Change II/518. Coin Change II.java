0d482b6079eb5c07e29ca3c384d5ad242c69a488
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-08-11 11:19:56 +0600

    Coin Change II.java

---
+++ LeetCode Problems/501 to 600/518. Coin Change II/518. Coin Change II.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int [][]dp = new int[n + 1][amount + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                if (coins[i - 1] > j)
+                    dp[i][j] = dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+            }
+        }
+        return dp[n][amount];
+    }
+}
