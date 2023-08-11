cdf65ced9f0c9f6876540c4cbd87d1a465c23531
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-08-11 11:19:49 +0600

    Coin Change II.cpp

---
+++ LeetCode Problems/501 to 600/518. Coin Change II/518. Coin Change II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount+1));
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                if (coins[i - 1] > j)
+                    dp[i][j] = dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+            }
+        }
+        return dp[n][amount];
+    }
+};
