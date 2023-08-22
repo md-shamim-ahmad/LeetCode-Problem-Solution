06794e97542376e75fb5a84444134f110fdb915e
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-08-22 22:12:53 +0600

    Excel Sheet Column Title.cpp

---
+++ LeetCode Problems/101 to 200/168. Excel Sheet Column Title/168. Excel Sheet Column Title.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string s = "", str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        while(columnNumber > 0) { 
+            s += str[(columnNumber - 1) % 26];
+            columnNumber = (columnNumber - 1) / 26;
+        }
+        reverse(s.begin(), s.end());
+        return s;
+    }
+};
