ea51705785129c61678e9f0b6ca798cd63ef0e01
Md Shamim Ahmad <56692841+md-shamim-ahmad@users.noreply.github.com>
2023-08-29 17:27:41 +0600

    2483. Minimum Penalty for a Shop Solution Added

---
+++ LeetCode Problems/2400 to 2500/2483. Minimum Penalty for a Shop/2483. Minimum Penalty for a Shop.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+        int n = 0 /*number of 'N' char*/, y = 0 /*number of 'Y' char*/;
+        for (char ch : customers) {
+            if (ch == 'N')
+                n++;
+        }
+        int penalty = INT_MAX, closing_time, i;
+        for (i = customers.size() - 1; i >= 0; i--) {
+            if (penalty >= n + y) {
+                penalty = n + y;
+                closing_time = i + 1;
+            }
+            if (customers[i] == 'Y')
+                y++;
+            if (customers[i] == 'N')
+                n--;
+        }
+        if (penalty >= n + y) {            
+            closing_time = i + 1;
+        }
+        return closing_time;
+    }
+};
