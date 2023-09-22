1a18ff6b9775f2e381f6ba8d6b8be39ac5390055
Md Shamim Ahmad <56692841+md-shamim-ahmad@users.noreply.github.com>
2023-09-22 07:08:21 +0600

    392. Is Subsequence Java Solution added

---
+++ LeetCode Problems/301 to 400/392. Is Subsequence/392. Is Subsequence.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int  j = 0;
+        for (int i = 0; i < t.length() && j < s.length(); i++) {
+            if (t.charAt(i) == s.charAt(j)) j++;
+        }
+        if (j == s.length()) return true;
+        return false;
+    }
+}
