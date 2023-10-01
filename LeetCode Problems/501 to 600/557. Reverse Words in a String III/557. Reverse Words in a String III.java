91d1fc4ba0a3372200a55be1ad5bf46ca07b0478
Md Shamim Ahmad <56692841+md-shamim-ahmad@users.noreply.github.com>
2023-10-01 08:54:58 +0600

    557. Reverse Words in a String III

---
+++ LeetCode Problems/501 to 600/557. Reverse Words in a String III/557. Reverse Words in a String III.java
@@ -0,0 +1,21 @@
+class Solution {
+    private void reverse(char []s, int l, int r) {
+        while(l < r) {
+            char temp = s[l];
+            s[l++] = s[r];
+            s[r--] = temp;            
+        }
+    }
+    public String reverseWords(String s) {
+        char []res = s.toCharArray();
+        for (int i = 0, l = 0; i < res.length; i++) {
+            if (res[i] == ' ') {
+                reverse(res, l, i - 1);
+                l = i + 1;
+            } else if (i == res.length - 1) {
+                reverse(res, l, i);
+            }
+        }
+        return new String(res);
+    }
+}
