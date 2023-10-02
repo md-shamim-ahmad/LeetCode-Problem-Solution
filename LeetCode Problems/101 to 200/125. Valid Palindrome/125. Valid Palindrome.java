1db5813212d67d9c8b151d1c089d25c2d58adc90
Avinash Chhetri <103208764+whitebeard10@users.noreply.github.com>
2023-10-02 21:49:19 +0530

    added java solution for porblem 125.Valid palindrome

---
+++ LeetCode Problems/101 to 200/125. Valid Palindrome/125. Valid Palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        // Convert to lowercase and remove non-alphanumeric characters
+        StringBuilder cleaned = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                cleaned.append(Character.toLowerCase(c));
+            }
+        }
+
+        // Check if the cleaned string is a palindrome
+        int left = 0;
+        int right = cleaned.length() - 1;
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
