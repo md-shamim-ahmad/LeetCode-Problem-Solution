e8a1242f7ca5a4a9180df5851015e827ee8783a9
Md Shamim Ahmad <56692841+md-shamim-ahmad@users.noreply.github.com>
2023-10-02 07:47:21 +0600

    Remove Colored Pieces if Both Neighbors are the Same Color Solution

---
+++ LeetCode Problems/2001 to 2100/2038. Remove Colored Pieces if Both Neighbors are the Same Color/2038. Remove Colored Pieces if Both Neighbors are the Same Color.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool winnerOfGame(string colors) {
+        int A = 0, B = 0;
+        for (int i = 1; i < colors.size() - 1; i++) {
+            if (colors[i] == colors[i - 1] && colors[i] == colors[i + 1]) {                    
+                if (colors[i] == 'A')
+                    A++;
+                else 
+                    B++;
+            }
+        }
+        if(A > B) 
+            return true;
+        return false;
+    }
+};
