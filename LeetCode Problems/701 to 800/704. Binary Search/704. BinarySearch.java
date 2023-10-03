c9295fabb6fbffdb4377a5574930b8352a568dc1
Satvik Yadav <satvikydv@gmail.com>
2023-10-04 01:06:06 +0530

    reformatted code

--- LeetCode Problems/701 to 800/704. Binary Search/704. BinarySearch.java
+++ LeetCode Problems/701 to 800/704. Binary Search/704. BinarySearch.java
@@ -1,21 +1,18 @@
-public class Solution{
+public class Solution {
     public static int search(int[] arr, int target) {
-        return findRequiredElement(arr, target, 0, arr.length-1);
+        return findRequiredElement(arr, target, 0, arr.length - 1);
     }
 
-    static int findRequiredElement(int[] arr, int target, int start, int end){
-
-        if (start<=end) {
+    static int findRequiredElement(int[] arr, int target, int start, int end) {
+        if (start <= end) {
             int mid = start + (end - start) / 2;
-
-            if(target == arr[mid]){
+            if (target == arr[mid]) {
                 return mid;
-            } else if(target < arr[mid]){
-                return find(arr, target, start,mid-1);
+            } else if (target < arr[mid]) {
+                return findRequiredElement(arr, target, start, mid - 1);
             } else {
-                return find(arr, target, mid+1, end);
+                return findRequiredElement(arr, target, mid + 1, end);
             }
-
         }
         return -1;
     }
