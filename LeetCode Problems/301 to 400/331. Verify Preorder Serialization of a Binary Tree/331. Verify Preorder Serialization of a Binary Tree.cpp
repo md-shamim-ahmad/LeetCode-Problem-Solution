075a08f1400a7d44002dd44e87d44ea1c0b30053
Anubhav Ghosh <88537612+Anubhav-Ghosh1@users.noreply.github.com>
2023-10-03 21:55:23 +0530

    Update 331. Verify Preorder Serialization of a Binary Tree.cpp

--- LeetCode Problems/301 to 400/331. Verify Preorder Serialization of a Binary Tree/331. Verify Preorder Serialization of a Binary Tree.cpp
+++ LeetCode Problems/301 to 400/331. Verify Preorder Serialization of a Binary Tree/331. Verify Preorder Serialization of a Binary Tree.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    bool isValidSerialization(string a) {
-        int i,j,k,c=1,s=0;
-        if(a.size()==0 )
+    bool isValidSerialization(string str) {
+        int count=1;
+        if(str.size()==0 )
         return 1;
 
-        if(a[0]=='#' and a.size()!=1)
+        if(str[0]=='#' and str.size()!=1)
         return 0;
 
-        for(i=0;i<a.size();i+=1){
-            c-=1;
-            if(c<0)
+        for(int i=0;i<str.size();i+=1){
+            count-=1;
+            if(count<0)
             return 0;
 
-            if(a[i]!='#'){
+            if(str[i]!='#'){
                 c+=2;
             }
             
             
             i++;
-            while(i<a.size() and a[i]!=','){
+            while(i<str.size() and str[i]!=','){
                 i++;
             }
         }
         cout<<c;
 
-        if(c==0)return 1;
+        if(count==0)return 1;
 
         return 0;
         
