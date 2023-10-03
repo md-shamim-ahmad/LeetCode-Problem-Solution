21ee30c8123978a3dc0c1a9b9c95196d902968e5
Vineetttt <vineetchotaliya30@gmail.com>
2023-10-03 13:30:13 +0530

    Adding solutions to problems-102,141

---
+++ LeetCode Problems/101 to 200/141. Linked List Cycle/Linked List Cycle.java
@@ -0,0 +1,33 @@
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+class Solution {
+    /*
+        Floyd’s Cycle Detection Algorithm
+        Time Complexity -> O(n)
+        Space Complexoty -> O(1)
+        
+        Move the slow pointer by one position.
+        Move the fast pointer by two positions.
+        If both pointers meet at some point then a loop exists and if the fast pointer meets the end position then no loop exists.
+    */
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+  
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) 
+                return true;
+            }
+            return false;
+        }
+}
