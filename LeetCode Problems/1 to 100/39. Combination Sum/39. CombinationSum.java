6ba140d8f90629c828d2bf6d42ebb6efd467b9a5
Monachaudhary <manuchaudhary991@gmail.com>
2023-10-03 14:12:15 -0700

    Java solution to Combination sum problem|Runtime 1ms|Fastest among all java users

--- LeetCode Problems/1 to 100/39. Combination Sum/39. CombinationSum.java
+++ LeetCode Problems/1 to 100/39. Combination Sum/39. CombinationSum.java
@@ -1,7 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Stack<Integer> stack = new 
+        List<List<Integer>> ans = new ArrayList<>();
+        allCombinations(0, candidates, target, ans, new ArrayList<>());
+        return ans;
 
-        
+    }
+
+    private void allCombinations(int i, int[] candidates, int target, List<List<Integer>> ans,
+            ArrayList<Integer> arrayList) {
+        if (i == candidates.length) {
+            if (target == 0) {
+                ans.add(new ArrayList<>(arrayList));
+            }
+
+            return;
+        }
+        if (target == 0) {
+            ans.add(new ArrayList<>(arrayList));
+            return;
+        }
+        if (candidates[i] <= target) {
+            arrayList.add(candidates[i]);
+            allCombinations(i, candidates, target - candidates[i], ans, arrayList);
+            arrayList.remove(arrayList.size() - 1);
+        }
+        allCombinations(i + 1, candidates, target, ans, arrayList);
     }
 }
