c5f30a5c3b3671366cab7d73c44c703be26abe7c
Vineetttt <vineetchotaliya30@gmail.com>
2023-10-03 09:29:05 +0530

    Adding java,cpp solution to 73. Set Matrix Zeroes

--- LeetCode Problems/1 to 100/73. Set Matrix Zeroes/73. Set Matrix Zeroes.java
+++ LeetCode Problems/1 to 100/73. Set Matrix Zeroes/73. Set Matrix Zeroes.java
@@ -1,5 +1,58 @@
 class Solution {
+    
+    /*
+    Time Complexity -> O(2*(N*M))
+    Space Complexity -> O(1)
+    */
+    
+    public int[][] setZeroesHelper(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int col0 = 1;
+        // Step 1: Traverse the matrix and mark 1st row & col accordingly:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    // Mark i-th row:
+                    matrix[i][0] = 0;
+
+                    // Mark j-th column:
+                    if (j != 0)
+                        matrix[0][j] = 0;
+                    else
+                        col0 = 0;
+                }
+            }
+        }
+
+        // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] != 0) {
+                    // Check for col & row:
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        // Step 3: Finally mark the 1st col & then 1st row:
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        return matrix;
+    }
+
     public void setZeroes(int[][] matrix) {
-        
+        matrix = setZeroesHelper(matrix);
     }
 }
