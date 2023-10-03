10afa1f09662fb25dc046dc7195c2f9374c6f131
avish107 <127240745+batman107@users.noreply.github.com>
2023-10-03 23:16:37 +0530

    Update 347. Top K Frequent Elements.cpp

--- LeetCode Problems/301 to 400/347. Top K Frequent Elements/347. Top K Frequent Elements.cpp
+++ LeetCode Problems/301 to 400/347. Top K Frequent Elements/347. Top K Frequent Elements.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        
+        unordered_map<int,int>mp;
+        priority_queue<pair<int,int>>pq;
+        for(auto i:nums){
+            mp[i]++;
+        }
+        nums.clear();
+        for(auto i:mp){
+            pq.push({i.second,i.first});
+        }
+        for(int i=0;i<k;i++){
+            nums.push_back(pq.top().second);
+            pq.pop();
+        }
+        return nums;
     }
 };
