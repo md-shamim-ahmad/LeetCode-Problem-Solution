402aee653b85b68bc62f70dd65d39ba3da28659c
Anubhav Ghosh <88537612+Anubhav-Ghosh1@users.noreply.github.com>
2023-10-03 18:35:39 +0530

    Update SingleNumber3.cpp

--- LeetCode Problems/200-300/SingleNumber 3/SingleNumber3.cpp
+++ LeetCode Problems/200-300/SingleNumber 3/SingleNumber3.cpp
@@ -1,19 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        vector<int>answer;
-        unordered_map<int,int>map;
-        for(auto i:nums)
-        {
+        vector<int> answer;
+        unordered_map<int, int> map;
+        
+        // Count the occurrences of each number in nums
+        for (auto i : nums) {
             map[i]++;
         }
-        for(auto i:map)
-        {
-            if(i.second == 1)
-            {
+        
+        // Find numbers with a count of 1 and add them to the answer vector
+        for (auto i : map) {
+            if (i.second == 1) {
                 answer.push_back(i.first);
             }
         }
+        
         return answer;
     }
 };
+
+int main() {
+    Solution solution;
+    
+    // Example usage:
+    vector<int> nums = {4, 3, 2, 4, 1, 3};
+    vector<int> result = solution.singleNumber(nums);
+    
+    cout << "Single Numbers are: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
