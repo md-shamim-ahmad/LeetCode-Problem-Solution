9a2e3d5ccdf1cf8efdbfd24e3d3e65175a9079a9
Monachaudhary <manuchaudhary991@gmail.com>
2023-10-04 15:29:17 -0700

    Changes made

--- LeetCode Problems/101 to 200/143. Reorder List/Reorder List.java
+++ LeetCode Problems/101 to 200/143. Reorder List/Reorder List.java
@@ -35,22 +35,4 @@ public class ReorderList {
         }
         return prev;
     }
-
-    public class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
 }
