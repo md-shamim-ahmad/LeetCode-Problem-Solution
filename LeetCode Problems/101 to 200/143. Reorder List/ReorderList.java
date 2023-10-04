d94633a55c1fade1a5974f8f7c9059dc5a42f736
Manu Chaudhary <96776726+Monachaudhary@users.noreply.github.com>
2023-10-04 00:14:11 -0700

    ReorderList.java

--- LeetCode Problems/101 to 200/143. Reorder List/ReorderList.java
+++ LeetCode Problems/101 to 200/143. Reorder List/ReorderList.java
@@ -36,21 +36,6 @@ public class ReorderList {
         return prev;
     }
 
-    public class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
+  
 
 }
