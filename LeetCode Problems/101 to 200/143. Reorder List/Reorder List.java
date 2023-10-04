c5b7a07bda03557ebd27f4c0ecadd8a337236a79
Manu Chaudhary <96776726+Monachaudhary@users.noreply.github.com>
2023-10-04 06:16:48 -0700

    Update Reorder List.java

--- LeetCode Problems/101 to 200/143. Reorder List/Reorder List.java
+++ LeetCode Problems/101 to 200/143. Reorder List/Reorder List.java
@@ -2,7 +2,7 @@ class Solution {
     public void reorderList(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
-        while(fast !=null && fast.next !=null){
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
         }
@@ -12,24 +12,24 @@ class Solution {
         slow.next = null;
         ListNode first = head;
         //combining both the lists
-        while(second!=null){
-             ListNode temp1 = first.next;
-             ListNode temp2 = second.next;
-             first.next= second;
-             second.next = temp1;
-             first = temp1;
-             second = temp2;
+        while (second != null) {
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+            first.next = second;
+            second.next = temp1;
+            first = temp1;
+            second = temp2;
         }
     }
-   private ListNode reverse(ListNode head){
+    private ListNode reverse(ListNode head) {
         ListNode prev = null;
         ListNode current = head;
         ListNode nxt = null;
-        while(current!=null){
+        while (current != null) {
             nxt = current.next;
             current.next = prev;
-            prev= current;
-            current= nxt;
+            prev = current;
+            current = nxt;
         }
         return prev;
     }
