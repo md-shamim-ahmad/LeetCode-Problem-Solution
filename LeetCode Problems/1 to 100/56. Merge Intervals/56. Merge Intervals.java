54110a31ad2c7b145c161ca1b3f577d5f8b19a25
Monachaudhary <manuchaudhary991@gmail.com>
2023-10-03 18:00:53 -0700

    Solution of Merge Interval problem in java(56)

--- LeetCode Problems/1 to 100/56. Merge Intervals/56. Merge Intervals.java
+++ LeetCode Problems/1 to 100/56. Merge Intervals/56. Merge Intervals.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 class Solution {
     public int[][] merge(int[][] intervals) {
-        
+        int n = intervals.length;
+        Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
+        List<int[]> list = new ArrayList<>();
+        if (n == 1) {
+            return intervals;
+        }
+        int[] interval = intervals[0];
+        list.add(interval);
+        for (int[] interv : intervals) {
+            if (interv[0] <= interval[1]) {
+                interval[1] = Math.max(interval[1], interv[1]);
+
+            } else {
+                interval = interv;
+                list.add(interval);
+            }
+        }
+        return list.toArray(new int[list.size()][]);
+
     }
 }
