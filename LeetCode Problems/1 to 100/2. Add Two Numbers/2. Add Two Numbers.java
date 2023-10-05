0dc7f9524f005d44bb0662b8419651d0c8b8172b
Deepa <deepa.c@peimedia.com>
2023-10-05 20:10:17 +0530

    Rename file and comment the class

--- LeetCode Problems/1 to 100/2. Add Two Numbers/2. Add Two Numbers.java
+++ LeetCode Problems/1 to 100/2. Add Two Numbers/2. Add Two Numbers.java
@@ -22,19 +22,19 @@ class Solution {
     }
 }
 
-public class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
+//public class ListNode {
+//    int val;
+//    ListNode next;
+//
+//    ListNode() {
+//    }
+//
+//    ListNode(int val) {
+//        this.val = val;
+//    }
+//
+//    ListNode(int val, ListNode next) {
+//        this.val = val;
+//        this.next = next;
+//    }
+//}
