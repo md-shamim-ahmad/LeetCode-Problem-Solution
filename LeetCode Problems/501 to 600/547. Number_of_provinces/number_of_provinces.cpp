f8dd343440dd0b8d2bf8d1b801e2a3bf2e27cb36
Saurabh Singh <96985651+SaurabhSingh720@users.noreply.github.com>
2023-10-06 10:41:33 +0530

    Update number_of_provinces.cpp

--- LeetCode Problems/501 to 600/547. Number_of_provinces/number_of_provinces.cpp
+++ LeetCode Problems/501 to 600/547. Number_of_provinces/number_of_provinces.cpp
@@ -1,31 +1,41 @@
 
-class Solution {
+class Solution
+{
 public:
-    void dfs(int j,vector<bool> &vis,vector<int> adj[]){
-        vis[j]=true;
-        for(auto x:adj[j]){
-            if(!vis[x]){
-                dfs(x,vis,adj);
+    void dfs(int j, vector<bool> &vis, vector<int> adj[])
+    {
+        vis[j] = true;
+        for (auto x : adj[j])
+        {
+            if (!vis[x])
+            {
+                dfs(x, vis, adj);
             }
         }
     }
-    int findCircleNum(vector<vector<int>>& v) {
-        int n=(int)v.size();
-        const int N=444;
+    int findCircleNum(vector<vector<int>> &v)
+    {
+        int n = (int)v.size();
+        const int N = 444;
         vector<int> adj[N];
         vector<bool> vis(N);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(v[i][j]==1 and i!=j){
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (v[i][j] == 1 and i != j)
+                {
                     adj[i].push_back(j);
                 }
             }
         }
-        vis.assign(n+4,false);
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,vis,adj);
+        vis.assign(n + 4, false);
+        int cnt = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (!vis[i])
+            {
+                dfs(i, vis, adj);
                 cnt++;
             }
         }
