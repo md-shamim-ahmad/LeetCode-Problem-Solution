d19d659e02ff1deba3f51058bc98896367f933b1
Nikhil Arora <nickzarora@gmail.com>
2023-10-10 15:44:00 +0530

    Updated format

--- LeetCode Problems/1 to 100/7. Reverse Integer/7. Reverse Integer.cs
+++ LeetCode Problems/1 to 100/7. Reverse Integer/7. Reverse Integer.cs
@@ -1,25 +1,30 @@
-public class Solution {
-    public int Reverse(int x) {
+public class Solution
+{
+    public int Reverse(int x)
+    {
         int num = 0;
         bool isNumberNegative = false;
-        if(x < 0){
+        if (x < 0)
+        {
             isNumberNegative = true;
             x = x * (-1);
         }
-        
+
         string number = x.ToString();
-        var array  = number.ToCharArray().Reverse();
-        number = string.Join("",array);
-        
-        try{
+        var array = number.ToCharArray().Reverse();
+        number = string.Join("", array);
+
+        try
+        {
             num = Convert.ToInt32(number);
         }
-        catch(Exception e){
+        catch (Exception e)
+        {
             num = 0;
         }
-        
-        if(isNumberNegative) num = num * (-1);
-        
+
+        if (isNumberNegative) num = num * (-1);
+
         return num;
     }
 }
