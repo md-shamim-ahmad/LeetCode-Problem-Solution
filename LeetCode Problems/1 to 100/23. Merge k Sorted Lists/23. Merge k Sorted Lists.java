32debfdc6da8c6c5740b785484f9438319d60ae4
Deepa <deepa.c@peimedia.com>
2023-10-11 21:53:01 +0530

    Fix formatting

--- LeetCode Problems/1 to 100/23. Merge k Sorted Lists/23. Merge k Sorted Lists.java
+++ LeetCode Problems/1 to 100/23. Merge k Sorted Lists/23. Merge k Sorted Lists.java
@@ -1,7 +1,6 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists.length == 0)
-            return null;
+        if (lists.length == 0) return null;
         ListNode result = lists[0];
         int i = 1;
         while (i < lists.length) {
@@ -12,10 +11,8 @@ class Solution {
     }
 
     ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null)
-            return list2;
-        if (list2 == null)
-            return list1;
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
 
         if (list1.val < list2.val) {
             list1.next = mergeTwoLists(list1.next, list2);
