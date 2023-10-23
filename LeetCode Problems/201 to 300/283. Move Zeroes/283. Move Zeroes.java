900d57c0c5f8356920e1fdfa25a8c51c8a88419c
Satvik Yadav <satvikydv@gmail.com>
2023-10-23 12:12:25 +0530

    made changes and added README file

--- LeetCode Problems/201 to 300/283. Move Zeroes/283. Move Zeroes.java
+++ LeetCode Problems/201 to 300/283. Move Zeroes/283. Move Zeroes.java
@@ -1,15 +1,11 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int k = 0;
-
-        // checking for the non zero elements
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
                 nums[k++] = nums[i];
             }
         }
-
-        // remaining elements will be zero
         for (int i = k; i < nums.length; i++) {
             nums[i] = 0;
         }
