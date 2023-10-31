b1ce654356c77974cf34b1b2099380e545a1d8d8
Vineetttt <vineetchotaliya30@gmail.com>
2023-10-31 15:08:14 +0530

    update clang format

--- LeetCode Problems/301 to 400/371. Sum of Two Integers/371. Sum of Two Integers.java
+++ LeetCode Problems/301 to 400/371. Sum of Two Integers/371. Sum of Two Integers.java
@@ -1,13 +1,13 @@
 class Solution {
     public int getSum(int a, int b) {
-       if (a == 0) return b;
-       if (b == 0) return a;
+        if (a == 0) return b;
+        if (b == 0) return a;
        
-       while (b != 0)  {
-           int carry = (a & b) << 1;
-           a = a ^ b;
-           b = carry; // reassign the carry into b
-       }
-       return a;
-   }
+        while (b != 0){
+            int carry = (a & b) << 1;
+            a = a ^ b;
+            b = carry; 
+        }
+        return a;
+    }
 }
