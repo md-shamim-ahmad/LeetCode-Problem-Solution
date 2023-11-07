bd4ee802c48a18925ff48a1ad3dd76b7f92beba1
Md Shamim Ahmad <56692841+md-shamim-ahmad@users.noreply.github.com>
2023-11-07 09:21:51 +0600

    Update 371. Sum of Two Integers.java

--- LeetCode Problems/301 to 400/371. Sum of Two Integers/371. Sum of Two Integers.java
+++ LeetCode Problems/301 to 400/371. Sum of Two Integers/371. Sum of Two Integers.java
@@ -2,11 +2,10 @@ class Solution {
     public int getSum(int a, int b) {
         if (a == 0) return b;
         if (b == 0) return a;
-       
-        while (b != 0){
+        while (b != 0) {
             int carry = (a & b) << 1;
             a = a ^ b;
-            b = carry; 
+            b = carry;
         }
         return a;
     }
