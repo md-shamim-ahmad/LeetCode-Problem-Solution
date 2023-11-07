9a27286a61724151afc54f78601fb8fbced56930
Md Shamim Ahmad <56692841+md-shamim-ahmad@users.noreply.github.com>
2023-11-07 09:11:02 +0600

    Update 10. Regular Expression Matching.cpp

--- LeetCode Problems/1 to 100/10. Regular Expression Matching/10. Regular Expression Matching.cpp
+++ LeetCode Problems/1 to 100/10. Regular Expression Matching/10. Regular Expression Matching.cpp
@@ -1,23 +1,16 @@
-class Solution
-{
+class Solution {
 public:
-    bool isMatch(string s, string p)
-    {
+    bool isMatch(string s, string p) {
         int sizeofString1 = s.length(), sizeOfString2 = p.length();
         bool dp[sizeofString1 + 1][sizeOfString2 + 1];
         memset(dp, false, sizeof(dp));
         dp[0][0] = true;
 
-        for (int i = 0; i <= sizeofString1; i++)
-        {
-            for (int j = 1; j <= sizeOfString2; j++)
-            {
-                if (p[j - 1] == '*')
-                {
+        for (int i = 0; i <= sizeofString1; i++) {
+            for (int j = 1; j <= sizeOfString2; j++) {
+                if (p[j - 1] == '*') {
                     dp[i][j] = dp[i][j - 2] || (i > 0 && (s[i - 1] == p[j - 2] || p[j - 2] == '.') && dp[i - 1][j]);
-                }
-                else
-                {
+                } else {
                     dp[i][j] = i > 0 && dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
                 }
             }
