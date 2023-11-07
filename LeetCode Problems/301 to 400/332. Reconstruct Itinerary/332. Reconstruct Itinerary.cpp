d43c1bb1d86760e974ff45c555c94badbd524dba
Md Shamim Ahmad <56692841+md-shamim-ahmad@users.noreply.github.com>
2023-11-07 12:13:29 +0600

    Update 332. Reconstruct Itinerary.cpp

--- LeetCode Problems/301 to 400/332. Reconstruct Itinerary/332. Reconstruct Itinerary.cpp
+++ LeetCode Problems/301 to 400/332. Reconstruct Itinerary/332. Reconstruct Itinerary.cpp
@@ -1,26 +1,19 @@
-class Solution
-{
+class Solution {
 public:
-    vector<string>  findItinerary(vector<vector<string>> &tickets)
-    {
+    vector<string> findItinerary(vector<vector<string>> &tickets) {
         unordered_map<string, priority_queue<string, vector<string>, greater<string>>> graph;
-        for (const auto &ticket : tickets)
-        {
+        for (const auto &ticket: tickets) {
             graph[ticket[0]].push(ticket[1]);
         }
         vector<string> newItinerary;
         stack<string> st;
         st.push("JFK");
-        while (!st.empty())
-        {
+        while (!st.empty()) {
             string currentAirport = st.top();
-            if (graph.find(currentAirport) != graph.end() && !graph[currentAirport].empty())
-            {
+            if (graph.find(currentAirport) != graph.end() && !graph[currentAirport].empty()) {
                 st.push(graph[currentAirport].top());
                 graph[currentAirport].pop();
-            }
-            else
-            {
+            } else {
                 newItinerary.push_back(st.top());
                 st.pop();
             }
