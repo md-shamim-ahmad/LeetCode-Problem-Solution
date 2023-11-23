f7b67bfe9a9fcd1ce75dba38f2e2c50c96aa90fe
Md Shamim Ahmad <56692841+md-shamim-ahmad@users.noreply.github.com>
2023-11-23 10:20:13 +0600

    Update 1814. Count Nice Pairs in an Array.cpp

--- LeetCode Problems/1801 to 1900/1814. Count Nice Pairs in an Array/1814. Count Nice Pairs in an Array.cpp
+++ LeetCode Problems/1801 to 1900/1814. Count Nice Pairs in an Array/1814. Count Nice Pairs in an Array.cpp
@@ -1,9 +1,9 @@
 class Solution {
-private:    
+private:
     int reverse(int num) {
         int ans = 0;
-        while(num > 0) {            
-            ans = ans *10 + num % 10;
+        while (num > 0) {
+            ans = ans * 10 + num % 10;
             num /= 10;
         }
         return ans;
@@ -11,19 +11,20 @@ private:
     long long calculatePair(long long n) {
         return (n * (n - 1)) / 2;
     }
+
 public:
-    int countNicePairs(vector<int>& nums) {
+    int countNicePairs(vector<int> &nums) {
         const int inf = 1000000007;
         vector<int> ress;
-        
-        for(int i = 0; i < nums.size(); i++) {            
+
+        for (int i = 0; i < nums.size(); i++) {
             ress.push_back(nums[i] - reverse(nums[i]));
         }
         sort(ress.begin(), ress.end());
         int ans = 0;
-        for(int i = 0; i < ress.size() - 1; i++) {
+        for (int i = 0; i < ress.size() - 1; i++) {
             long long cnt = 1;
-            while(i < ress.size() - 1 && ress[i] == ress[i + 1]) {
+            while (i < ress.size() - 1 && ress[i] == ress[i + 1]) {
                 cnt++;
                 i++;
             }
