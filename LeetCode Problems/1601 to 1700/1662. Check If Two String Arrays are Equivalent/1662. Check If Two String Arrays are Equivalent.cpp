73c0c416364b5df69907aa27487f56778c03cf6c
Md Shamim Ahmad <56692841+md-shamim-ahmad@users.noreply.github.com>
2023-12-01 10:28:51 +0600

    1662. Check If Two String Arrays are Equivalent Solution Added

--- LeetCode Problems/1601 to 1700/1662. Check If Two String Arrays are Equivalent/1662. Check If Two String Arrays are Equivalent.cpp
+++ LeetCode Problems/1601 to 1700/1662. Check If Two String Arrays are Equivalent/1662. Check If Two String Arrays are Equivalent.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string> &word1, vector<string> &word2) {
-        for (int i = 1; i < word1.size(); i++) {
-            word1[0] += word1[i];
-        }
-        for (int i = 1; i < word2.size(); i++) {
-            word2[0] += word2[i];
-        }
-        return word1[0] == word2[0];
+        string s1 = "", s2 = "";
+        for (auto s: word1) s1 += s;
+        for (auto s: word2) s2 += s;
+        return s1 == s2;
     }
 };
