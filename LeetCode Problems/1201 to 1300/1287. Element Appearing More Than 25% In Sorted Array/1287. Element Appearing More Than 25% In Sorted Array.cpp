5cf691b4bb4b03f4abb72f16d3bf9a1a03720ce9
Md Shamim Ahmad <56692841+md-shamim-ahmad@users.noreply.github.com>
2023-12-11 16:40:07 +0600

    1287. Element Appearing More Than 25% In Sorted Array Solution Added (#137)

---
+++ LeetCode Problems/1201 to 1300/1287. Element Appearing More Than 25% In Sorted Array/1287. Element Appearing More Than 25% In Sorted Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findSpecialInteger(vector<int> &arr) {
+        int mx = -1, pos = 0, n = arr.size();
+        for (int i = 1; i < n; i++) {
+            int cnt = 0;
+            while (i + 1 <= n && arr[i] == arr[i - 1]) {
+                cnt++, i++;
+            }
+            if (cnt > mx) {
+                mx = cnt;
+                pos = i - 1;
+            }
+        }
+        return arr[pos];
+    }
+};
