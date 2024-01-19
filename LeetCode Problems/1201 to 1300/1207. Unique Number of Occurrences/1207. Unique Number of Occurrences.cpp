d0008cb21f07a5744068cffba6cd977337abe5b8
Md Shamim Ahmad <56692841+md-shamim-ahmad@users.noreply.github.com>
2024-01-19 17:15:30 +0600

    Update 1207. Unique Number of Occurrences.cpp

--- LeetCode Problems/1201 to 1300/1207. Unique Number of Occurrences/1207. Unique Number of Occurrences.cpp
+++ LeetCode Problems/1201 to 1300/1207. Unique Number of Occurrences/1207. Unique Number of Occurrences.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     bool uniqueOccurrences(vector<int> &arr) {
         map<int, int> mp;
-        for (auto it: arr)
-            mp[it]++;
-        arr.clear();
-        for (auto it: mp)
-            arr.push_back(it.second);
-        mp.clear();
-        for (auto it: arr) {
-            if (mp[it] == 1)
-                return false;
-            mp[it] = 1;
+        for (int i = 0; i < arr.size(); i++) {
+            mp[arr[i]]++;
         }
-        return true;
+        set<int> s;
+        for (auto it: mp) {
+            s.insert(it.second);
+        }
+        return s.size() == mp.size();
     }
 };
