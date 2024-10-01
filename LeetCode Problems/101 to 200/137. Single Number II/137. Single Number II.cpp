2bafeb0f414a263bacd386a3473a0094ef438a65
Harshendra M <151964309+harshendram@users.noreply.github.com>
2024-10-01 18:20:14 +0530

    Update 137. Single Number II.cpp
    I propose updating the implementation to use the XOR bitwise operation, which provides a more efficient solution. The XOR approach has a time complexity of O(n) but reduces the space complexity to O(1), as it does not require any additional data structures.

--- LeetCode Problems/101 to 200/137. Single Number II/137. Single Number II.cpp
+++ LeetCode Problems/101 to 200/137. Single Number II/137. Single Number II.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
-    int singleNumber(vector<int> &nums) {
-        map<int, int> mp;
-        for (auto ele: nums) {
-            mp[ele]++;
+    int singleNumber(vector<int>& nums) {
+        int xorr=0;
+        for(int i=0;i<nums.size();i++){
+            xorr =xorr^nums[i];
         }
-        for (auto it: mp) {
-            if (it.second == 1)
-                return it.first;
-        }
-        return 0;
+        return xorr;
+        
     }
 };
+
+// class Solution {
+// public:
+//     int singleNumber(vector<int> &nums) {
+//         map<int, int> mp;
+//         for (auto ele: nums) {
+//             mp[ele]++;
+//         }
+//         for (auto it: mp) {
+//             if (it.second == 1)
+//                 return it.first;
+//         }
+//         return 0;
+//     }
+// };
+
