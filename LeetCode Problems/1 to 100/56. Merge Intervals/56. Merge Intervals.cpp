802d8b7a8e03fc4849326189a6c28b70bf2be219
bastenahamad <bastenahamad@gmail.com>
2024-10-07 23:12:23 +0530

    Added 3 Problems in Blind-75

--- LeetCode Problems/1 to 100/56. Merge Intervals/56. Merge Intervals.cpp
+++ LeetCode Problems/1 to 100/56. Merge Intervals/56. Merge Intervals.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>> &intervals) {
+    vector<vector<int>> merge(vector<vector<int>> &ar) {
+        sort(ar.begin(), ar.end());
+        vector<vector<int>> ans;
+        for (int i = 0; i < ar.size(); i++) {
+            if (ans.size() == 0 or ans.back()[1] < ar[i][0]) {
+                ans.push_back({ar[i][0], ar[i][1]});
+            } else
+                ans.back()[1] = max(ans.back()[1], ar[i][1]);
+        }
+        return ans;
     }
 };
--- LeetCode Problems/1 to 100/91. Decode Ways/91. Decode Ways.cpp
+++ LeetCode Problems/1 to 100/91. Decode Ways/91. Decode Ways.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+    int n;
+    vector<int> dp;
+    int rec(int ind, string s) {
+        if (ind == n) return 1;
+        if (dp[ind] != -1) return dp[ind];
+        if (s[ind] == '0') return 0;
+        int ans = rec(ind + 1, s);
+        if (ind + 1 < n and (s[ind] == '1' or (s[ind] == '2' and s[ind + 1] <= '6'))) {
+            ans += rec(ind + 2, s);
+        }
+        dp[ind] = ans;
+        return ans;
+    }
     int numDecodings(string s) {
+        n = s.size();
+        dp.resize(n, -1);
+        return rec(0, s);
     }
 };
