319914d747d1b9695f5860418c469d656356ef75
Md Shamim Ahmad <56692841+md-shamim-ahmad@users.noreply.github.com>
2024-10-07 22:29:28 +0600

    Revert "Update SingleNumber Implementation to Use Bitwise XOR #143 (#144)"
    This reverts commit 8dca650f9ab03ccdd318e13759eac64287184940.

--- LeetCode Problems/101 to 200/137. Single Number II/137. Single Number II.cpp
+++ LeetCode Problems/101 to 200/137. Single Number II/137. Single Number II.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
     int singleNumber(vector<int> &nums) {
-        int xorr = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            xorr = xorr ^ nums[i];
+        map<int, int> mp;
+        for (auto ele: nums) {
+            mp[ele]++;
         }
-        return xorr;
+        for (auto it: mp) {
+            if (it.second == 1)
+                return it.first;
+        }
+        return 0;
     }
 };
-
-// class Solution {
-// public:
-//     int singleNumber(vector<int> &nums) {
-//         map<int, int> mp;
-//         for (auto ele: nums) {
-//             mp[ele]++;
-//         }
-//         for (auto it: mp) {
-//             if (it.second == 1)
-//                 return it.first;
-//         }
-//         return 0;
-//     }
-// };
